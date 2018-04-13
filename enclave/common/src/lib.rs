158532779de6c649d70cf136a32747ebe1194038
Yawning Angel <yawning@schwanenlied.me>
2018-04-13 09:37:55 +0000

    Validate Intel's signature on the AVR reports.

--- enclave/common/src/lib.rs
+++ enclave/common/src/lib.rs
@@ -4,9 +4,12 @@ extern crate sgx_types;
 
 extern crate base64;
 extern crate byteorder;
+extern crate pem_iterator;
+extern crate percent_encoding;
 extern crate protobuf;
 extern crate serde_json;
 extern crate sodalite;
+extern crate webpki;
 
 #[macro_use]
 extern crate ekiden_common;
