785c894f082b0b20314ff1ca598a9144291997cf
Warren He <w@oasislabs.com>
2018-05-04 19:20:31 +0000

    deployment: only skip AVR verification in simulation

--- docker/deployment/build-images.sh
+++ docker/deployment/build-images.sh
@@ -3,6 +3,11 @@
 ekiden_image=${EKIDEN_DOCKER_IMAGE:-ekiden/development:0.1.0-alpha.4}
 base_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )
 build_sgx_mode=${BUILD_IMAGES_SGX_MODE:-SIM}
+if [ "$build_sgx_mode" = SIM ]; then
+    build_skip_avr_verify_flag="-e EKIDEN_SKIP_AVR_VERIFY=1"
+else
+    build_skip_avr_verify_flag=""
+fi
 
 cd ${base_dir}
 
@@ -14,7 +19,7 @@ elif [ -z "$BUILD_IMAGES_CONTAINER" ]; then
         -v "$PWD:/code" \
         -e "SGX_MODE=$build_sgx_mode" \
         -e INTEL_SGX_SDK=/opt/sgxsdk \
-        -e EKIDEN_UNSAFE_SKIP_AVR_VERIFY=1 \
+        $build_skip_avr_verify_flag \
         -w /code \
         "$ekiden_image" \
         /code/docker/deployment/build-images-inner.sh
