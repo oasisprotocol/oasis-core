1ecab3d06d3a8703aa26aed5cb602926d919c6ff
Yawning Angel <yawning@schwanenlied.me>
2018-05-04 00:26:50 +0000

    Add Send/Sync annotations to the new glue interface traits.

--- beacon/base/src/backend.rs
+++ beacon/base/src/backend.rs
@@ -5,7 +5,7 @@ use ekiden_common::epochtime::EpochTime;
 use ekiden_common::futures::BoxFuture;
 
 /// Random Beacon backend implementing the Ekiden random beacon interface.
-pub trait RandomBeacon {
+pub trait RandomBeacon: Send + Sync {
     /// Queries the random beacon implementation for the specified epoch's
     /// value.  Output will not be returned for epochs in the future.
     fn get_beacon(&self, epoch: EpochTime) -> BoxFuture<B256>;
