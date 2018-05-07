d54b1dd36c61772ffdb5d9939b02ce9d3220adbe
Will Scott <will@cypherpunk.email>
2018-05-07 13:52:55 -0700

    Add a service adapter to allow storage backends to act as gRPC services

--- storage/base/src/lib.rs
+++ storage/base/src/lib.rs
@@ -1,6 +1,10 @@
 //! Ekiden storage interface.
 extern crate ekiden_common;
+extern crate ekiden_storage_api;
+extern crate grpcio;
 
 pub mod backend;
+pub mod service;
 
 pub use backend::*;
+pub use service::*;
