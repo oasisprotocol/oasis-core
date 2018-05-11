3aac2647d100b9f97750a966b5456ae8e5cee564
Will Scott <will@cypherpunk.email>
2018-05-11 14:06:31 -0700

    API types

--- scheduler/api/src/lib.rs
+++ scheduler/api/src/lib.rs
@@ -1,7 +1,11 @@
 extern crate futures;
 extern crate grpcio;
 extern crate protobuf;
+extern crate ekiden_common_api;
+
+use ekiden_common_api as common;
 
 mod generated;
 
 pub use generated::scheduler::*;
+pub use generated::scheduler_grpc::*;
