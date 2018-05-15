2f4e4b1d1afc127d0163e4fb28ae41b8ff56de78
Bennet Yee <bsy@oasislabs.com>
2018-05-15 12:00:50 -0700

    Initial proto file for stake escrow.
    The proto file has been tested with a direct invocation of protoc.
    The cargo ekiden build-contract command wants the *.rs files with the
    rest of the plumbing, which does not exist yet.

---
+++ escrow/api/src/lib.rs
@@ -0,0 +1,14 @@
+extern crate futures;
+extern crate grpcio;
+extern crate protobuf;
+
+extern crate ekiden_common_api;
+
+mod generated;
+
+use ekiden_common_api as common;
+
+pub use generated::stake::*;
+// pub use generated::contract_grpc::*;
+// pub use generated::entity::*;
+// pub use generated::entity_grpc::*;
