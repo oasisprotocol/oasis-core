7990aa7480707535994df0769fa0e64edb03c3db
Bennet Yee <bsy@oasislabs.com>
2018-05-28 21:54:15 -0700

    documentation for backend interface

--- stake/base/src/stake_backend.rs
+++ stake/base/src/stake_backend.rs
@@ -18,24 +18,53 @@ pub struct StakeStatus {
     pub escrowed: AmountType,    // ... of which this much is tied up in escrow.
 }
 
-/// Stake escrow backend implementing the Ekiden stake escrow interface.
+/// Stake escrow backend implementing the Ekiden stake/escrow
+/// interface.  The AmountType parameters marked ($$) are intended to
+/// represent actual token transfers; the other AmountType parameters
+/// are requests for values, or status, and do not represent token
+/// transfers.
 pub trait StakeEscrowBackend: Send + Sync {
-    /// Stake
+    /// Deposits |amount| ($$) into the stake account for |msg_sender|.
     fn deposit_stake(&self, msg_sender: B256, amount: AmountType) -> BoxFuture<()>;
+
+    /// Returns the stake account status (StakeStatus) of the caller |msg_sender|.
     fn get_stake_status(&self, msg_sender: B256) -> BoxFuture<StakeStatus>;
+
+    /// Withdraws |amount_requested| ($$) from the stake account
+    /// belonging to the caller |msg_sender|.  The value
+    /// |amount_requested| cannot exceed available funds (e.g.,
+    /// |total_stake - escrowed|) in the stake account.
     fn withdraw_stake(
         &self,
         msg_sender: B256,
         amount_requested: AmountType,
     ) -> BoxFuture<AmountType>;
+
+    /// Allocates |escrow_amount| from the caller (|msg_sender|)'s
+    /// stake account to create a new stake account with |target| as
+    /// the escrow target.  The stake/escrow service will keep the
+    /// funds in escrow until |target| invokes take_and_release_escrow
+    /// on the escrow account id returned by this call.
     fn allocate_escrow(
         &self,
         msg_sender: B256,
         target: B256,
         escrow_amount: AmountType,
     ) -> BoxFuture<B256>;
+
+    /// Returns a vector of all active escrow accounts created by |msg_sender|.
     fn list_active_escrows(&self, msg_sender: B256) -> BoxFuture<Vec<api::EscrowData>>;
+
+    /// Returns the escrow account data associated with a given |escrow_id|.
     fn fetch_escrow_by_id(&self, escrow_id: B256) -> BoxFuture<api::EscrowData>;
+
+    /// Dissolves the escrow account |escrow_id|: the |msg_sender|
+    /// must be the target of the escrow account, and
+    /// |amount_requested| of the escrow amount is returned ($$) to
+    /// the caller (e.g., forfeiture).  Any remaining amount is marked
+    /// as available in the stake account of the creator of the escrow
+    /// account, i.e., it is released from escrow and returned back to
+    /// the owner.
     fn take_and_release_escrow(
         &self,
         msg_sender: B256,
