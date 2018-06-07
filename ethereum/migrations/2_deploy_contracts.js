4ea2223a8c5f532906717d7508210f22fa59072f
Yawning Angel <yawning@schwanenlied.me>
2018-06-07 03:56:07 +0000

    ethereum/MockEpoch: Add a mock epoch solidity contract.
    This follows the existing `epochtime::MockTimeSource` semantics, with an event
    emitted on the `set_epoch()` call to make implementing the notifier
    possible.

--- ethereum/migrations/2_deploy_contracts.js
+++ ethereum/migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
 var RandomBeacon = artifacts.require("./RandomBeacon.sol");
+var MockEpoch = artifacts.require("./MockEpoch.sol");
 var OasisEpoch = artifacts.require("./OasisEpoch.sol");
 
 module.exports = function (deployer) {
     deployer.deploy(OasisEpoch).then(function() {
+        return deployer.deploy(MockEpoch);
+    }).then(function() {
         return deployer.deploy(RandomBeacon, OasisEpoch.address);
     });
 };
