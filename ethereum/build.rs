154d4d3db0ee34f6cb18809c82429e3363cfcee4
Bennet Yee <bsy@oasislabs.com>
2018-06-07 15:48:55 -0700

    Removed per contract old truffle infrastructure and move to new.

--- ethereum/build.rs
+++ ethereum/build.rs
@@ -7,5 +7,7 @@ fn main() {
         .status()
         .expect("truffle failed to build");
 
+    println!("cargo:rerun-if-changed={}", "contracts/Migrations.sol");
     println!("cargo:rerun-if-changed={}", "contracts/RandomBeacon.sol");
+    println!("cargo:rerun-if-changed={}", "contracts/Stake.sol");
 }
