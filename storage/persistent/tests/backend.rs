dc91f0e6545d8d5498659febb621d5773e084d3d
Will Scott <will@oasislabs.com>
2018-06-19 14:26:49 -0700

    basic Insert benchmarks for dummy / sled

--- storage/persistent/tests/backend.rs
+++ storage/persistent/tests/backend.rs
@@ -1,9 +1,14 @@
+#![feature(test)]
+
 extern crate ekiden_common;
 extern crate ekiden_epochtime;
 extern crate ekiden_storage_base;
 extern crate ekiden_storage_persistent;
 
+extern crate test;
+
 use std::fs;
+use test::Bencher;
 
 use ekiden_common::futures::Future;
 use ekiden_epochtime::local::SystemTimeSource;
@@ -21,6 +26,20 @@ fn test_persistent_backend() {
     assert!(backend.get(key).wait().is_err());
     backend.insert(b"value".to_vec(), 10).wait().unwrap();
     assert_eq!(backend.get(key).wait(), Ok(b"value".to_vec()));
+
+    fs::remove_dir_all(db_path).expect("Could not cleanup DB.");
+}
+
+#[bench]
+fn bench_persistent_speed(b: &mut Bencher) {
+    use ekiden_storage_base::StorageBackend;
+
+    let db_path = String::from("./db/");
+    let backend = PersistentStorageBackend::new(Box::new(SystemTimeSource {}), &db_path);
+    assert!(!backend.is_err());
+    let backend = backend.unwrap();
+
+    b.iter(|| backend.insert(b"value".to_vec(), 10).wait().unwrap());
 
     fs::remove_dir_all(db_path).expect("Could not cleanup DB.");
 }
