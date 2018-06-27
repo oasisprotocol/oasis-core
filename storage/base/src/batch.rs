201a8aeb66237c1654f6dfd94affb237172b59c6
Warren He <w@oasislabs.com>
2018-06-27 13:52:29 -0700

    storage: address review comments

--- storage/base/src/batch.rs
+++ storage/base/src/batch.rs
@@ -7,9 +7,19 @@ use super::backend::StorageBackend;
 /// persisted. This interface adds methods for demarcating a batch of writes and waiting for them
 /// all to be persisted.
 pub trait BatchStorage: StorageBackend {
-    /// Start collecting a batch of writes. You can only collect into one batch at a time.
+    /// Start collecting a batch of writes. You can only collect into one batch at a time. Calling
+    /// this while already collecting a batch is an error.
+    ///
+    /// # Panics
+    ///
+    /// If called while already collecting a batch, this may panic.
     fn start_batch(&self);
     /// Stop collecting the current batch of writes. Returns a future that resolves after the
-    /// the writes in the batch are persisted.
+    /// the writes in the batch are persisted. Calling this while not collecting a batch is an
+    /// error.
+    ///
+    /// # Panics
+    ///
+    /// If called while not collecting a batch, this may panic.
     fn end_batch(&self) -> BoxFuture<()>;
 }
