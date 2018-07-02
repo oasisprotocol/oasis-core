ff04fde64684c60d83c768fcca9e3deee5378464
Will Scott <will@oasislabs.com>
2018-07-02 14:38:43 -0700

    Wait for the provider to "start up" before calling it.

--- ethereum/test/web3.js
+++ ethereum/test/web3.js
@@ -4,26 +4,43 @@
  * send 0-value transactions to itself at a fixed interval.
  */
 
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var web3 = require('web3');
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
-
-var makeTxn = function () {
-    client.eth.sendTransaction({
-        from: "627306090abab3a6e1400e9345bc60c78a8bef57",
-        gas: 100000,
-        gasPrice: 0
+var pause = async function (timeout) {
+    return new Promise(function (resolve, _reject) {
+        setInterval(resolve, timeout);
     });
 };
 
-// If directly executed.
+var makeTxn = async function (client) {
+    return new Promise(function (resolve, _reject) {
+        client.eth.sendTransaction({
+            from: "627306090abab3a6e1400e9345bc60c78a8bef57",
+            gas: 100000,
+            gasPrice: 0
+        }, resolve);
+    });
+};
+
 if (require.main === module) {
+    var HDWalletProvider = require("truffle-hdwallet-provider");
+    var web3 = require('web3');
+    var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
     if (process.argv.length < 4) {
         console.warn("Usage: web3.js <provider> <interval-ms>");
-        exit(0);
+        process.exit(0);
     }
-    var provider = new HDWalletProvider(mnemonic, process.argv[2]);
-    var client = new web3();
-    client.setProvider(provider);
-    setInterval(makeTxn.bind(client), process.argv[3]);
+
+    let startup = async function () {
+        var provider = new HDWalletProvider(mnemonic, process.argv[2]);
+        var client = new web3();
+        await client.setProvider(provider);
+        return client;
+    };
+    let loop = async function (client) {
+        await pause(process.argv[3]);
+        await makeTxn(client);
+        return loop(client);
+    }
+
+    startup().then(loop);
 }
