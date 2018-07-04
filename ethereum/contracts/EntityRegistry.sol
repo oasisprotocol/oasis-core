3e1d6d8c7221ab57edb2de89b351ee0e3d3a6e6b
Will Scott <will@oasislabs.com>
2018-07-04 12:11:00 -0700

    registry tracks addresses; links stake

--- ethereum/contracts/EntityRegistry.sol
+++ ethereum/contracts/EntityRegistry.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.23;
 
 import "./EpochABI.sol";
+import "./Stake.sol";
 
 contract EntityRegistry {
     event Entity(address indexed _from, bytes32 indexed id, uint64 epoch);
@@ -8,9 +9,16 @@ contract EntityRegistry {
     event Node(address indexed _from, bytes32 indexed id, uint64 epoch);
 
     EpochContract epoch_source;
+    Stake stake_source;
 
-    constructor(address epoch_addr) public {
+    address[] public nodes;
+    // The reverse-map into nodes, to allow for efficient deletion.
+    // Note: one-indexed, because 0-values are the same as unset.
+    mapping(address => uint64) node_idxs;
+
+    constructor(address epoch_addr, address stake_addr) public {
         epoch_source = EpochContract(epoch_addr);
+        stake_source = Stake(stake_addr);
     }
 
     function() public {
@@ -18,17 +26,35 @@ contract EntityRegistry {
     }
 
     function register(bytes32 id) public {
+        // TODO: validate stake.
         (uint64 epoch, , ) = epoch_source.get_epoch(uint64(block.timestamp));
         emit Entity(msg.sender, id, epoch);
     }
 
     function deregister(bytes32 id) public {
         (uint64 epoch, , ) = epoch_source.get_epoch(uint64(block.timestamp));
+
+        // TODO: require deregistration in an epoch after registration.
+        // TODO: require address has no stake in jeopardy at time of dereg.
+        uint64 offset = node_idxs[msg.sender];
+        if (offset != 0) {
+            nodes[offset - 1] = nodes[nodes.length - 1];
+            delete nodes[nodes.length - 1];
+            nodes.length--;
+            node_idxs[nodes[offset - 1]] = offset;
+            node_idxs[msg.sender] = 0;
+        }
         emit Dereg(msg.sender, id, epoch);
     }
 
     function registerNode(bytes32 node) public {
+        // TODO: validate stake.
         (uint64 epoch, , ) = epoch_source.get_epoch(uint64(block.timestamp));
+
+        require(node_idxs[msg.sender] == 0);
+        require(nodes.length < 0xFFFFFFFFFFFFFFFF);
+        node_idxs[msg.sender] = uint64(nodes.push(msg.sender));
+
         emit Node(msg.sender, node, epoch);
     }
 }
