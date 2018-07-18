1227ea4fa6c5eaa4df7f1c9047ea92784aa62d8b
Jernej Kos <jernej@kos.mx>
2018-07-18 10:37:51 +0200

    Fix ci-changed.sh script when run on CI
    When running it on CI the master branch is not checked when running
    builds from other branches, so the diff is always empty. This commit
    fixes this by running diff against origin/master instead.

--- scripts/ci-changed.sh
+++ scripts/ci-changed.sh
@@ -7,7 +7,7 @@ fi
 # when writing arguments, separate them with regex for grep
 IFS="|"
 MATCH="$*"
-if [[ $(git diff --name-only master | grep -P "^($MATCH)") ]]; then
+if [[ $(git diff --name-only origin/master | grep -P "^($MATCH)") ]]; then
   echo "Repo diverged from master. Running CI Job"
   exit 0
 else
