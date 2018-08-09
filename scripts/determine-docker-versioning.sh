9c31e277e3e46bf2f939e0b303fe0eafd664383d
Reuven V Gonzales <ravenac95@users.noreply.github.com>
2018-08-08 23:45:29 -0700

    Rename versioning.sh

--- scripts/determine-docker-versioning.sh
+++ scripts/determine-docker-versioning.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+##
+# This is a build script to determine the docker tag to use for versioning the
+# docker image. This will write to an output file that can be used as an artifact
+# between build steps
+#
+# Usage determine-docker-versioning.sh [path-to-output-file]
+##
+
 set -euxo pipefail
 
 timestamp=`date +%Y%m%d%H%M%S` # YYYYmmddHHMMSS
@@ -10,3 +18,7 @@ else
     # Otherwise this is a pull-request and use the sha1
     echo "export DOCKER_TAG=${CIRCLE_SHA1}" >> $BASH_ENV
 fi
+
+# Write the docker tag out to a specific file so we can
+# save it as an artifact for the build
+echo $DOCKER_TAG > $1
