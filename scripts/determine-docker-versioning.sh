5bcecf856b5245bdbb5bdd6f43cc9f5340d3353c
Reuven V Gonzales <ravenac95@users.noreply.github.com>
2018-08-09 18:43:36 -0700

    Fix docker versioning

--- scripts/determine-docker-versioning.sh
+++ scripts/determine-docker-versioning.sh
@@ -13,12 +13,13 @@ timestamp=`date +%Y%m%d%H%M%S` # YYYYmmddHHMMSS
 
 if [ "$CIRCLE_BRANCH" = "master" ]; then
     # If the current branch is master then use the git-tag and a timestamp
-    echo "export DOCKER_TAG=${CIRCLE_TAG}-${timestamp}" >> $BASH_ENV
+    export DOCKER_TAG=${CIRCLE_TAG}-${timestamp}
 else
     # Otherwise this is a pull-request and use the sha1
-    echo "export DOCKER_TAG=${CIRCLE_SHA1}" >> $BASH_ENV
+    export DOCKER_TAG=${CIRCLE_SHA1}
 fi
 
 # Write the docker tag out to a specific file so we can
 # save it as an artifact for the build
 echo $DOCKER_TAG > $1
+printf "export DOCKER_TAG=%s\n" "$DOCKER_TAG" >> $BASH_ENV
