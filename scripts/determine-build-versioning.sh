51970fce8e89d34782cc9aaf963ca5666fd26ad5
Reuven V. Gonzales <ravenac95@users.noreply.github.com>
2018-08-11 21:08:32 -0700

    CI Versioning (#784)
    * Setting up basic versioned container artifacts
    
    * Make executable
    
    * Moved to script execution
    
    * Need to checkout first
    
    * Always deploy docker container
    
    * Always deploys the txclient
    
    * Fix workflow
    
    * config.yml fix
    
    * Another config.yml fix
    
    * Rename versioning.sh
    
    * Update config.yml
    
    Updates the config.yml to support versioning releases and production promotions.
    
    * Typo: should be cat not echo
    
    * Fix management of workspace
    
    * Fix docker versioning
    
    * Fix deps
    
    * Allow paralell docker deploys
    
    * Editor introduced tabs
    
    * Need to checkout
    
    * Add docker label
    
    * Properly add sha labels
    
    * Handle approval correctly
    
    * Typo fix
    
    * Fix docker build call
    
    * Run a test of the hold
    
    * Fix filter
    
    * Properly setup prod tagging
    
    * Typo fix
    
    * Fake commit to trigger CI
    
    * Setup for prod
    
    * A typo fix
    
    * Rename tag
    
    * Finish renaming things
    
    * Don't build txclient on large

---
+++ scripts/determine-build-versioning.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+##
+# This is a build script to determine the docker tag to use for versioning the
+# docker image. This will write to an output file that can be used as an artifact
+# between build steps
+#
+# Usage determine-docker-versioning.sh [path-to-output-file]
+##
+
+set -euxo pipefail
+
+timestamp=`date +%Y%m%d%H%M%S` # YYYYmmddHHMMSS
+
+if [ "$CIRCLE_BRANCH" = "master" ]; then
+    # If the current branch is master then use the git-tag and a timestamp
+    export BUILD_IMAGE_TAG=${CIRCLE_TAG}-${timestamp}
+else
+    # Otherwise this is a pull-request and use the sha1
+    export BUILD_IMAGE_TAG=${CIRCLE_SHA1}
+fi
+
+# Write the docker tag out to a specific file so we can
+# save it as an artifact for the build
+echo $BUILD_IMAGE_TAG > $1
+printf "export BUILD_IMAGE_TAG=%s\n" "$BUILD_IMAGE_TAG" >> $BASH_ENV
