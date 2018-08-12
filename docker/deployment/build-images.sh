95a3e3f2963ce2683be7abce9f07f378ffb1804d
Reuven V. Gonzales <ravenac95@users.noreply.github.com>
2018-08-11 21:36:28 -0700

    Revert "CI Versioning (#784)"
    This reverts commit 51970fce8e89d34782cc9aaf963ca5666fd26ad5.

--- docker/deployment/build-images.sh
+++ docker/deployment/build-images.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-ekiden_commit_sha=${CIRCLE_SHA1:unknown}
 ekiden_image=${EKIDEN_DOCKER_IMAGE:-ekiden/development:0.2.0}
 base_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )
 build_sgx_mode=${BUILD_IMAGES_SGX_MODE:-SIM}
@@ -31,4 +30,4 @@ else
 fi
 
 # Build the deployable image from the output.
-docker build --rm --force-rm --build-arg EKIDEN_COMMIT_SHA=$ekiden_commit_sha -t oasislabs/testnet:$BUILD_IMAGE_TAG - <target/docker-deployment/context.tar.gz
+docker build --rm --force-rm -t oasislabs/testnet - <target/docker-deployment/context.tar.gz
