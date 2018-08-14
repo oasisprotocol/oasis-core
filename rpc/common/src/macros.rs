52b90249617643b3d5311c0b4825336f384b0398
Will Scott <will@cypherpunk.email>
2018-08-13 18:56:25 -0700

    ignore doc tests for instrumentation when testing

--- rpc/common/src/macros.rs
+++ rpc/common/src/macros.rs
@@ -11,7 +11,7 @@
 /// For example, if you want to create enclave glue from the given API, and
 /// have the `create_enclave_rpc` macro available, you can invoke it with this
 /// concrete API by doing:
-/// ```
+/// ```rust,ignore
 /// with_api! {
 ///     create_enclave_rpc!(api);
 /// }
@@ -33,7 +33,7 @@ macro_rules! rpc_api {
         /// For example, if you want to create enclave glue from the given API, and
         /// have the `create_enclave_rpc` macro available, you can invoke it with this
         /// concrete API by doing:
-        /// ```
+        /// ```rust,ignore
         /// with_api! {
         ///     create_enclave_rpc!(api);
         /// }
