d66efe4a54b34a110e39ecfc9e8c805170e7be75
Warren He <w@oasislabs.com>
2018-09-20 11:16:06 -0700

    go: allow custom agent address

--- go/ekiden/cmd/tracing.go
+++ go/ekiden/cmd/tracing.go
@@ -11,26 +11,30 @@ import (
 )
 
 const (
-	cfgTracingEnabled               = "tracing.enabled"
-	cfgTracingReporterFlushInterval = "tracing.reporter.flush-interval"
-	cfgTracingSamplerParam          = "tracing.sampler.param"
+	cfgTracingEnabled                    = "tracing.enabled"
+	cfgTracingReporterFlushInterval      = "tracing.reporter.flush-interval"
+	cfgTracingReporterLocalAgentHostPort = "tracing.reporter.agent-addr"
+	cfgTracingSamplerParam               = "tracing.sampler.param"
 )
 
 var (
-	tracingEnabled               bool
-	tracingReporterFlushInterval time.Duration
-	tracingSamplerParam          float64
+	tracingEnabled                    bool
+	tracingReporterFlushInterval      time.Duration
+	tracingReporterLocalAgentHostPort string
+	tracingSamplerParam               float64
 )
 
 func initTracing(cmd *cobra.Command, serviceName string) (io.Closer, error) {
 	enabled, _ := cmd.Flags().GetBool(cfgTracingEnabled)
 	reporterFlushInterval, _ := cmd.Flags().GetDuration(cfgTracingReporterFlushInterval)
+	reporterLocalAgentHostPort, _ := cmd.Flags().GetString(cfgTracingReporterLocalAgentHostPort)
 	samplerParam, _ := cmd.Flags().GetFloat64(cfgTracingSamplerParam)
 
 	cfg := config.Configuration{
 		Disabled: !enabled,
 		Reporter: &config.ReporterConfig{
 			BufferFlushInterval: reporterFlushInterval,
+			LocalAgentHostPort:  reporterLocalAgentHostPort,
 		},
 		Sampler: &config.SamplerConfig{
 			Param: samplerParam,
@@ -43,11 +47,13 @@ func initTracing(cmd *cobra.Command, serviceName string) (io.Closer, error) {
 func registerTracingFlags(cmd *cobra.Command) {
 	cmd.Flags().BoolVar(&tracingEnabled, cfgTracingEnabled, true, "Enable tracing")
 	cmd.Flags().DurationVar(&tracingReporterFlushInterval, cfgTracingReporterFlushInterval, 1*time.Second, "How often the buffer is force-flushed, even if it's not full")
+	cmd.Flags().StringVar(&tracingReporterLocalAgentHostPort, cfgTracingReporterLocalAgentHostPort, "localhost:6831", "Send spans to jaeger-agent at this address")
 	cmd.Flags().Float64Var(&tracingSamplerParam, cfgTracingSamplerParam, 0.001, "Probability for probabilistic sampler")
 
 	for _, v := range []string{
 		cfgTracingEnabled,
 		cfgTracingReporterFlushInterval,
+		cfgTracingReporterLocalAgentHostPort,
 		cfgTracingSamplerParam,
 	} {
 		_ = viper.BindPFlag(v, cmd.Flags().Lookup(v))
