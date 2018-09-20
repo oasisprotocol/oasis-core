f45d5702f4de797ab1005147b56e5119aac2bc6d
Warren He <w@oasislabs.com>
2018-09-20 14:05:37 -0700

    docker: non-fatal go prep in dirty deployment builds

--- docker/deployment/build-images-inner.sh
+++ docker/deployment/build-images-inner.sh
@@ -23,7 +23,7 @@ cargo install --force --path tools
 # Build all Ekiden Go binaries and resources.
 GO_SRC_BASE=${GOPATH}/src/github.com/oasislabs
 mkdir -p ${GO_SRC_BASE}
-ln -s `pwd` ${GO_SRC_BASE}/ekiden
+ln -sfT `pwd` ${GO_SRC_BASE}/ekiden
 (cd ${GO_SRC_BASE}/ekiden/go && dep ensure -v)
 (cd ${GO_SRC_BASE}/ekiden/go && go generate ./...)
 (cd ${GO_SRC_BASE}/ekiden/go && go build -o ./ekiden/ekiden ./ekiden)
