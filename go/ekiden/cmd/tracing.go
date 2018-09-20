dc1480676efc2ede7e70678665438cadc9e98bde
Warren He <w@oasislabs.com>
2018-09-20 10:43:08 -0700

    go: tracing.disabled -> tracing.enabled

--- go/ekiden/cmd/tracing.go
+++ go/ekiden/cmd/tracing.go
@@ -11,24 +11,24 @@ import (
 )
 
 const (
-	cfgTracingDisabled              = "tracing.disabled"
+	cfgTracingEnabled               = "tracing.enabled"
 	cfgTracingReporterFlushInterval = "tracing.reporter.flush-interval"
 	cfgTracingSamplerParam          = "tracing.sampler.param"
 )
 
 var (
-	tracingDisabled              bool
+	tracingEnabled               bool
 	tracingReporterFlushInterval time.Duration
 	tracingSamplerParam          float64
 )
 
 func initTracing(cmd *cobra.Command, serviceName string) (io.Closer, error) {
-	disabled, _ := cmd.Flags().GetBool(cfgTracingDisabled)
+	enabled, _ := cmd.Flags().GetBool(cfgTracingEnabled)
 	reporterFlushInterval, _ := cmd.Flags().GetDuration(cfgTracingReporterFlushInterval)
 	samplerParam, _ := cmd.Flags().GetFloat64(cfgTracingSamplerParam)
 
 	cfg := config.Configuration{
-		Disabled: disabled,
+		Disabled: !enabled,
 		Reporter: &config.ReporterConfig{
 			BufferFlushInterval: reporterFlushInterval,
 		},
@@ -41,12 +41,12 @@ func initTracing(cmd *cobra.Command, serviceName string) (io.Closer, error) {
 }
 
 func registerTracingFlags(cmd *cobra.Command) {
-	cmd.Flags().BoolVar(&tracingDisabled, cfgTracingDisabled, true, "Disable tracing")
+	cmd.Flags().BoolVar(&tracingEnabled, cfgTracingEnabled, true, "Enable tracing")
 	cmd.Flags().DurationVar(&tracingReporterFlushInterval, cfgTracingReporterFlushInterval, 1*time.Second, "How often the buffer is force-flushed, even if it's not full")
 	cmd.Flags().Float64Var(&tracingSamplerParam, cfgTracingSamplerParam, 0.001, "Probability for probabilistic sampler")
 
 	for _, v := range []string{
-		cfgTracingDisabled,
+		cfgTracingEnabled,
 		cfgTracingSamplerParam,
 	} {
 		_ = viper.BindPFlag(v, cmd.Flags().Lookup(v))
