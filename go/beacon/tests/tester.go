213faf489fc4001a7557fe96aa9c2d05e331b07e
Yawning Angel <yawning@schwanenlied.me>
2018-10-19 14:52:34 +0000

    go/beacon: Move the API tests to `tests/`

---
+++ go/beacon/tests/tester.go
@@ -0,0 +1,60 @@
+// Package tests is a collection of beacon implementation test cases.
+package tests
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/net/context"
+
+	"github.com/oasislabs/ekiden/go/beacon/api"
+	epochtime "github.com/oasislabs/ekiden/go/epochtime/api"
+)
+
+const recvTimeout = 1 * time.Second
+
+// BeaconImplementationTests exercises the basic functionality of a
+// beacon backend.
+func BeaconImplementationTests(t *testing.T, backend api.Backend, epochtime epochtime.SetableBackend) {
+	require := require.New(t)
+
+	epoch, _, err := epochtime.GetEpoch(context.Background())
+	require.NoError(err, "GetEpoch")
+
+	ch, sub := backend.WatchBeacons()
+	defer sub.Close()
+
+recvLoop:
+	for {
+		select {
+		case ev := <-ch:
+			// Skip the old beacon.
+			if ev.Epoch < epoch {
+				continue
+			}
+			require.Equal(epoch, ev.Epoch, "WatchBeacons - epoch")
+			require.Len(ev.Beacon, api.BeaconSize, "WatchBeacons - length")
+
+			var b []byte
+			b, err = backend.GetBeacon(context.Background(), epoch)
+			require.NoError(err, "GetBeacon")
+			require.Equal(b, ev.Beacon, "GetBeacon - beacon")
+			break recvLoop
+		case <-time.After(recvTimeout):
+			t.Fatalf("failed to receive current beacon on WatchBeacons")
+		}
+	}
+
+	epoch++
+	err = epochtime.SetEpoch(context.Background(), epoch, 0)
+	require.NoError(err, "SetEpoch")
+
+	select {
+	case ev := <-ch:
+		require.Equal(epoch, ev.Epoch, "WatchBeacons - epoch")
+		require.Len(ev.Beacon, api.BeaconSize, "WatchBeacons - length")
+	case <-time.After(recvTimeout):
+		t.Fatalf("failed to receive current beacon after transition")
+	}
+}
