928648180251f2fd3289a98963f9b9560afa47d0
Yawning Angel <yawning@schwanenlied.me>
2018-10-24 11:03:31 +0000

    registry/runtime: Remove the StoreID field
    This is unused, and there are no plans in the future to use such a
    thing.
    
    WARNING: This change is BACKWARDS INCOMPATIBLE.
    
    Fixes #1087.

--- go/registry/api/runtime_test.go
+++ go/registry/api/runtime_test.go
@@ -12,7 +12,6 @@ func TestSerialization(t *testing.T) {
 	key, _, _ := ed25519.GenerateKey(nil)
 	c := Runtime{
 		ID:                       signature.PublicKey(key),
-		StoreID:                  StoreID{100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131},
 		Code:                     []byte{0x12, 0x13, 0x14, 0x15, 0x16},
 		MinimumBond:              42,
 		ModeNonDeterministic:     false,
