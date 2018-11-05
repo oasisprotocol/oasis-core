8de87bb0ecca6459cfe62e70613c4364341f1d4f
Jernej Kos <jernej@kos.mx>
2018-11-05 13:41:44 +0100

    common/futures: Add block_on helper function

--- common/src/futures/mod.rs
+++ common/src/futures/mod.rs
@@ -5,6 +5,8 @@ pub use self::extern_futures::*;
 
 use super::error::Error;
 
+#[cfg(not(target_env = "sgx"))]
+mod block_on;
 mod killable;
 mod retry;
 mod select_all;
@@ -116,6 +118,8 @@ pub trait StreamExt: Stream {
 
 impl<S: Stream> StreamExt for S {}
 
+#[cfg(not(target_env = "sgx"))]
+pub use self::block_on::block_on;
 pub use self::killable::{killable, KillHandle};
 pub use self::retry::retry;
 #[cfg(not(target_env = "sgx"))]
