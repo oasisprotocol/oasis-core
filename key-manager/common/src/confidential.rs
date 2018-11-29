310e6bb4d5f74fc29cbe4715746f10253f2142ba
armaniferrante <armaniferrante@gmail.com>
2018-11-29 05:30:45 +0000

    Use key manager allocated keys

--- key-manager/common/src/confidential.rs
+++ key-manager/common/src/confidential.rs
@@ -1,28 +1,39 @@
-//! Encryption utilties for web3(c) V0.5 key management.
+//! Encryption utilties for Web3(c).
+//! Wraps the ekiden_core::mrae::sivaessha2 primitives with a set of encryption
+//! methods that transparently encodes/decodes the Web3(c) wire format.
 
 use ekiden_core::error::{Error, Result};
 use ekiden_core::mrae::sivaessha2;
 
 use super::{PrivateKeyType, PublicKeyType, EMPTY_PRIVATE_KEY, EMPTY_PUBLIC_KEY};
-use sodalite;
 
+/// Encrypts the given plaintext using the symmetric key derived from
+/// peer_public_key and secret_key. Uses the given public_key to return
+/// an encrypted payload of the form: nonce || public_key || cipher,
+/// Allowing the receipient of the encrypted payload to decrypt with
+/// the given nonce and public_key.
 pub fn encrypt(
     plaintext: Vec<u8>,
     nonce: Vec<u8>,
     peer_public_key: PublicKeyType,
+    public_key: &PublicKeyType,
+    secret_key: &PrivateKeyType,
 ) -> Result<Vec<u8>> {
-    let (public_key, secret_key) = default_contract_keys();
     let ciphertext = sivaessha2::box_seal(
         nonce.clone(),
         plaintext.clone(),
         vec![],
         peer_public_key.into(),
-        secret_key,
+        *secret_key,
     )?;
-    Ok(encode_encryption(ciphertext, nonce, public_key))
+    Ok(encode_encryption(ciphertext, nonce, *public_key))
 }
 
-pub fn decrypt(data: Option<Vec<u8>>) -> Result<Decryption> {
+/// Decrypts the given payload generated in the same manner by the encrypt method.
+/// I.e., given an encrypted payload of the form nonce || public_key || cipher,
+/// extracts the nonce and public key and uses them along with the given secret_key
+/// the decrypt the cipher, returning the resulting Decryption struct.
+pub fn decrypt(data: Option<Vec<u8>>, secret_key: &PrivateKeyType) -> Result<Decryption> {
     if data.is_none() {
         return Ok(Decryption {
             plaintext: Default::default(),
@@ -31,13 +42,12 @@ pub fn decrypt(data: Option<Vec<u8>>) -> Result<Decryption> {
         });
     }
     let (nonce, peer_public_key, cipher) = split_encrypted_payload(data.unwrap())?;
-    let (_, private_key) = default_contract_keys();
     let plaintext = sivaessha2::box_open(
         nonce.clone(),
         cipher,
         vec![],
         peer_public_key.into(),
-        private_key,
+        *secret_key,
     )?;
     Ok(Decryption {
         plaintext,
@@ -55,6 +65,7 @@ pub struct Decryption {
     pub peer_public_key: PublicKeyType,
 }
 
+/// Packs the given paramaters into a Vec of the form nonce || public_key || ciphertext.
 fn encode_encryption(
     mut ciphertext: Vec<u8>,
     nonce: Vec<u8>,
@@ -78,18 +89,4 @@ fn split_encrypted_payload(data: Vec<u8>) -> Result<(Vec<u8>, PublicKeyType, Vec
     peer_public_key.copy_from_slice(&data[nonce_size..nonce_size + 32]);
     let cipher = data[nonce_size + 32..].to_vec();
     Ok((nonce, peer_public_key, cipher))
-}
-
-/// Hard coded key manager retrieved contract keys for Web3(c) V0.5.
-/// Public key = 0x9385b8391e06d67c3de1675a58cffc3ad16bcf7cc56ab35d7db1fc03fb227a54.
-/// Private key = 0xd5af0c986e6a9cce52d05803e962d4b19f915905debcb41f35b68eebc954fa49.
-pub fn default_contract_keys() -> (PublicKeyType, PrivateKeyType) {
-    let seed = [
-        213, 175, 12, 152, 110, 106, 156, 206, 82, 208, 88, 3, 233, 98, 212, 177, 159, 145, 89, 5,
-        222, 188, 180, 31, 53, 182, 142, 235, 201, 84, 250, 73,
-    ];
-    let mut public_key = EMPTY_PUBLIC_KEY;
-    let mut private_key = EMPTY_PRIVATE_KEY;
-    sodalite::box_keypair_seed(&mut public_key, &mut private_key, &seed);
-    (public_key, private_key)
 }
