e105c32892db42e782b5baddccdea8780da925a1
Jernej Kos <jernej@kos.mx>
2019-01-03 13:15:55 +0100

    ci: Enable Go worker host tests
    Enabling these tests requires a functioning worker and runtime binaries
    which must therefore be built before the Go tests can run.

--- .buildkite/go/test_and_coverage_ekiden.sh
+++ .buildkite/go/test_and_coverage_ekiden.sh
@@ -11,6 +11,17 @@
 # https://buildkite.com/docs/pipelines/writing-build-scripts
 set -euxo pipefail
 
+source .buildkite/scripts/common.sh
+# NOTE: Need Rust common to initialize the SGX SDK libraries.
+source .buildkite/rust/common.sh
+
+# Setup worker and test runtime which is needed to test the worker host.
+download_artifact ekiden-worker target/debug 755
+download_artifact token.so target/enclave 755
+
+export EKIDEN_TEST_WORKER_HOST_WORKER_BINARY=$(pwd)/target/debug/ekiden-worker
+export EKIDEN_TEST_WORKER_HOST_RUNTIME_BINARY=$(pwd)/target/enclave/token.so
+
 ######################
 # Test the ekiden node
 ######################
