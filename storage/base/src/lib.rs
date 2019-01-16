b484bcc3b7577f06aa2be63d19a254334b371b27
Jernej Kos <jernej@kos.mx>
2019-01-16 11:50:56 +0100

    Cleanup some more now-unneeded Rust code

--- storage/base/src/lib.rs
+++ storage/base/src/lib.rs
@@ -1,22 +1,8 @@
 //! Ekiden storage interface.
-#[macro_use]
 extern crate ekiden_common;
-#[cfg(not(target_env = "sgx"))]
-extern crate ekiden_storage_api;
-#[cfg(not(target_env = "sgx"))]
-extern crate grpcio;
-#[cfg(not(target_env = "sgx"))]
-#[macro_use]
-extern crate log;
-#[cfg(not(target_env = "sgx"))]
-extern crate protobuf;
 
 pub mod backend;
 pub mod mapper;
-#[cfg(not(target_env = "sgx"))]
-pub mod service;
 
 pub use backend::*;
 pub use mapper::*;
-#[cfg(not(target_env = "sgx"))]
-pub use service::*;
