bbe62c18ca34f1dd042ec2522286f517243f53a7
Jernej Kos <jernej@kos.mx>
2019-01-16 14:41:36 +0100

    ci: Port roothash/storage migration tests to new Go worker

--- .buildkite/scripts/test_migration.sh
+++ .buildkite/scripts/test_migration.sh
@@ -4,8 +4,24 @@
 # This script tests the Ekiden rust project.
 #
 # Usage:
-# test_migration.sh
+# test_migration.sh [-w <workdir>]
 ############################################################
+
+# Defaults.
+WORKDIR=$(pwd)
+
+#########################
+# Process test arguments.
+#########################
+while getopts 'f:t:' arg
+do
+    case ${arg} in
+        w) WORKDIR=${OPTARG};;
+        *)
+            echo "Usage: $0 [-w <workdir>]"
+            exit 1
+    esac
+done
 
 # Helpful tips on writing build scripts:
 # https://buildkite.com/docs/pipelines/writing-build-scripts
@@ -15,72 +31,29 @@ source .buildkite/scripts/common.sh
 source .buildkite/scripts/common_e2e.sh
 source .buildkite/rust/common.sh
 
-# Working directory.
-WORKDIR=${1:-$(pwd)}
-
 # Runtime identifier.
 RUNTIME_ID=0000000000000000000000000000000000000000000000000000000000000000
 
+######################################
+# Storage and roothash migration test.
+######################################
 test_migration() {
-    local epochtime_backend=$1
-    local beacon_backend=$2
-    local registry_backend=$3
-    local roothash_backend=$4
+    local runtime=token
 
-    local datadir=/tmp/ekiden-dummy-data
-
-    # Start the first dummy network.
-    rm -rf "$datadir"
-    "$WORKDIR/go/ekiden/ekiden" \
-        --log.level debug \
-        --grpc.port 42261 \
-        --epochtime.backend "$epochtime_backend" \
-        --beacon.backend "$beacon_backend" \
-        --storage.backend memory \
-        --scheduler.backend trivial \
-        --registry.backend "$registry_backend" \
-        --roothash.backend "$roothash_backend" \
-        --tendermint.consensus.timeout_commit 250ms \
-        --datadir "$datadir" \
-        &
-    local first_dummy_pid=$(jobs -p +)
-
+    # Start the first network.
+    run_backend_tendermint_committee tendermint_mock 1
     sleep 1
 
-    "$WORKDIR/target/debug/ekiden-compute" \
-        --worker-path ${WORKDIR}/target/debug/ekiden-worker \
-        --worker-cache-dir $(mktemp -d) \
-        --no-persist-identity \
-        --max-batch-size 1 \
-        --storage-backend remote \
-        --port 10001 \
-        --disable-key-manager \
-        --test-runtime-id "$RUNTIME_ID" \
-        "$WORKDIR/target/enclave/token.so" \
-        &
-    local first_compute1_pid=$(jobs -p +)
+    run_compute_node 1 ${runtime} \
+        --worker.runtime.replica_group_size 1 \
+        --worker.runtime.replica_group_backup_size 0 &>/dev/null
 
-    "$WORKDIR/target/debug/ekiden-compute" \
-        --worker-path ${WORKDIR}/target/debug/ekiden-worker \
-        --worker-cache-dir $(mktemp -d) \
-        --no-persist-identity \
-        --max-batch-size 1 \
-        --storage-backend remote \
-        --port 10002 \
-        --disable-key-manager \
-        --test-runtime-id "$RUNTIME_ID" \
-        "$WORKDIR/target/enclave/token.so" \
-        &
-    local first_compute2_pid=$(jobs -p +)
+    wait_compute_nodes 1
+    set_epoch 1
+    sleep 1
 
-    sleep 3
-
-    "${WORKDIR}/go/ekiden/ekiden" debug dummy wait-nodes --nodes 2
-    "$WORKDIR/go/ekiden/ekiden" debug dummy set-epoch --epoch 1
-
-    sleep 2
-
-    # Start long term client, which has a 10-second wait.
+    # Start long term client, which has a 10-second wait. We run this client so
+    # that we test if the migration works without restarting the client.
     "$WORKDIR/target/debug/test-long-term-client" \
         --storage-backend remote \
         --mr-enclave "$(cat "$WORKDIR/target/enclave/token.mrenclave")" \
@@ -88,79 +61,47 @@ test_migration() {
         &
     local client_pid=$(jobs -p +)
 
-    sleep 2
-    # 2 sec
-
-    # Stop the network.
-    kill "$first_compute1_pid" "$first_compute2_pid"
-    wait "$first_compute1_pid" "$first_compute2_pid" || true
-
-    # Export.
-    "$WORKDIR/go/ekiden/ekiden" storage export --output_file /tmp/ekiden-test-storage.dat
-    "$WORKDIR/go/ekiden/ekiden" debug roothash export "$RUNTIME_ID" --output_file /tmp/ekiden-test-roothash.dat
-
-    # Finish tearing down the network.
-    kill -KILL "$first_dummy_pid"
-    wait "$first_dummy_pid" || true
-
-    sleep 1
-    # 3 sec
-
-    # Start the second dummy node.
-    rm -rf "$datadir"
-    "$WORKDIR/go/ekiden/ekiden" \
-        --log.level debug \
-        --grpc.port 42261 \
-        --epochtime.backend "$epochtime_backend" \
-        --beacon.backend "$beacon_backend" \
-        --storage.backend memory \
-        --scheduler.backend trivial \
-        --registry.backend "$registry_backend" \
-        --roothash.backend "$roothash_backend" \
-        --roothash.genesis_blocks /tmp/ekiden-test-roothash.dat \
-        --tendermint.consensus.timeout_commit 250ms \
-        --datadir "$datadir" \
-        &
-    local second_dummy_pid=$(jobs -p +)
-
-    sleep 1
+    sleep 4
     # 4 sec
 
-    # Import.
-    "$WORKDIR/go/ekiden/ekiden" storage import --input_file /tmp/ekiden-test-storage.dat --current_epoch 1
+    # Stop the compute nodes.
+    pkill --echo --full --signal 9 worker.backend
+
+    # Export.
+    "$WORKDIR/go/ekiden/ekiden" storage export \
+        --address "127.0.0.1:${EKIDEN_STORAGE_PORT}" \
+        --output_file ${TEST_BASE_DIR}/export-storage.dat
+    "$WORKDIR/go/ekiden/ekiden" debug roothash export "$RUNTIME_ID" --output_file ${TEST_BASE_DIR}/export-roothash.dat
+
+    # Stop the validator and storage nodes.
+    pkill --echo --full --signal 9 ekiden
+
+    sleep 1
+    # 5 sec
+
+    # Start the second network.
+    run_backend_tendermint_committee tendermint_mock 2 \
+        --roothash.genesis_blocks ${TEST_BASE_DIR}/export-roothash.dat
+
+    sleep 1
+    # 6 sec
+
+    # Import storage.
+    "$WORKDIR/go/ekiden/ekiden" storage import \
+        --address "127.0.0.1:${EKIDEN_STORAGE_PORT}" \
+        --input_file ${TEST_BASE_DIR}/export-storage.dat \
+        --current_epoch 1
 
     # Finish starting the second network.
-    "$WORKDIR/target/debug/ekiden-compute" \
-        --worker-path ${WORKDIR}/target/debug/ekiden-worker \
-        --worker-cache-dir $(mktemp -d) \
-        --no-persist-identity \
-        --max-batch-size 1 \
-        --storage-backend remote \
-        --port 10001 \
-        --disable-key-manager \
-        --test-runtime-id "$RUNTIME_ID" \
-        "$WORKDIR/target/enclave/token.so" \
-        &
-    local second_compute1_pid=$(jobs -p +)
-
-    "$WORKDIR/target/debug/ekiden-compute" \
-        --worker-path ${WORKDIR}/target/debug/ekiden-worker \
-        --worker-cache-dir $(mktemp -d) \
-        --no-persist-identity \
-        --max-batch-size 1 \
-        --storage-backend remote \
-        --port 10002 \
-        --disable-key-manager \
-        --test-runtime-id "$RUNTIME_ID" \
-        "$WORKDIR/target/enclave/token.so" \
-        &
-    local second_compute2_pid=$(jobs -p +)
+    run_compute_node 1 ${runtime} \
+        --worker.runtime.replica_group_size 1 \
+        --worker.runtime.replica_group_backup_size 0 &>/dev/null
 
     sleep 3
-    # 7 sec
+    # 9 sec
 
-    "${WORKDIR}/go/ekiden/ekiden" debug dummy wait-nodes --nodes 2
-    "$WORKDIR/go/ekiden/ekiden" debug dummy set-epoch --epoch 2
+    wait_compute_nodes 1
+    set_epoch 2
 
     # Wait on the client and check its exit status.
     wait "$client_pid"
@@ -169,5 +110,4 @@ test_migration() {
     cleanup
 }
 
-test_migration mock insecure memory memory
-test_migration tendermint_mock tendermint tendermint tendermint
+test_migration
