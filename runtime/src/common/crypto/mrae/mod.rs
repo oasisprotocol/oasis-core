fa4c53a433745bb1c60f8b8bfb79636971a8ebf3
Armani Ferrante <armaniferrante@gmail.com>
2019-04-09 11:44:23 -0700

    Update deoxysii crate name (#1571)

--- runtime/src/common/crypto/mrae/mod.rs
+++ runtime/src/common/crypto/mrae/mod.rs
@@ -2,7 +2,7 @@
 extern crate aes_soft as aes;
 extern crate block_modes;
 extern crate crypto_ops;
-extern crate deoxysii_rust;
+extern crate deoxysii as deoxysii_rust;
 extern crate ring;
 extern crate x25519_dalek;
 
