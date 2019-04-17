c3a516e5317e944679c605e309c60194e4e7b117
Yawning Angel <yawning@schwanenlied.me>
2019-04-17 10:06:38 +0000

    go: Add a placeholder package to reduce frustration

---
+++ go/doc.go
@@ -0,0 +1,5 @@
+// Package ekidengo contains the ekiden components written in Go, including
+// but not limited to the node binary.
+package ekidengo
+
+// This is a placeholder, there are many others like it, but this one is mine.
