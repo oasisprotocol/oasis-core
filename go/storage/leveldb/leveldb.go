221bf2898cf57b89bc9cb46fcbb072807c75cbe4
Jan Berčič <jan.bercic@gmail.com>
2019-05-09 16:09:30 +0200

    Fix storage Apply write log processing, it skipped deletions

--- go/storage/leveldb/leveldb.go
+++ go/storage/leveldb/leveldb.go
@@ -201,9 +201,12 @@ func (b *leveldbBackend) Apply(ctx context.Context, root hash.Hash, expectedNewR
 			return nil, err
 		}
 
-		// XXX(jan) what about deletions?
 		for _, entry := range log {
-			err = tree.Insert(entry.Key, entry.Value)
+			if len(entry.Value) == 0 {
+				err = tree.Remove(entry.Key)
+			} else {
+				err = tree.Insert(entry.Key, entry.Value)
+			}
 			if err != nil {
 				return nil, err
 			}
