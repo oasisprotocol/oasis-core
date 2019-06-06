3d1eb3e8396f6c226bcf096734bd94a5586ad72d
Tadej Janež <tadej.j@nez.si>
2019-06-06 16:51:20 +0200

    Improve StorageImplementationTests
    Use in-memory Urkel tree to calculate the expected new root.
    Refactor write log preparation and expected new root calculation to
    corresponding helper functions.
    Add tests for ApplyBatch() and GetValue() backend methods.

--- go/storage/tests/tester.go
+++ go/storage/tests/tester.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/oasislabs/ekiden/go/common/crypto/hash"
 	"github.com/oasislabs/ekiden/go/storage/api"
+	"github.com/oasislabs/ekiden/go/storage/mkvs/urkel"
 )
 
 var testValues = [][]byte{
@@ -19,32 +20,73 @@ var testValues = [][]byte{
 	[]byte("There shines not one shall leave alive the battlefield!"),
 }
 
-// StorageImplementationTests exercises the basic functionality of
-// a storage backend.
-func StorageImplementationTests(t *testing.T, backend api.Backend) {
-	<-backend.Initialized()
-
-	// Test MKVS storage.
+func prepareWriteLog(values [][]byte) api.WriteLog {
 	var wl api.WriteLog
 	for i, v := range testValues {
 		wl = append(wl, api.LogEntry{Key: []byte(strconv.Itoa(i)), Value: v})
 	}
+	return wl
+}
+
+func calculateExpectedNewRoot(t *testing.T, wl api.WriteLog) hash.Hash {
+	// Use in-memory Urkel tree to calculate the expected new root.
+	tree := urkel.New(nil, nil)
+	for _, logEntry := range wl {
+		err := tree.Insert(context.Background(), logEntry.Key, logEntry.Value)
+		require.NoError(t, err, "error inserting writeLog entry into Urkel tree")
+	}
+	_, expectedNewRoot, err := tree.Commit(context.Background())
+	require.NoError(t, err, "error calculating mkvs' expectedNewRoot")
+	return expectedNewRoot
+}
+
+// StorageImplementationTests exercises the basic functionality of a storage
+// backend.
+func StorageImplementationTests(t *testing.T, backend api.Backend) {
+	<-backend.Initialized()
+
+	// Test MKVS storage.
 	var root hash.Hash
 	root.Empty()
-	expectedNewRoot := [...]byte{82, 3, 202, 16, 125, 182, 175, 25, 51, 188, 131, 181, 118, 76, 249, 15, 53, 89, 59, 224, 95, 75, 239, 182, 157, 30, 80, 48, 237, 108, 90, 22}
+	wl := prepareWriteLog(testValues)
+	expectedNewRoot := calculateExpectedNewRoot(t, wl)
+	var mkvsReceipt *api.MKVSReceipt
+	var rb api.MKVSReceiptBody
+	var err error
 
-	mkvsReceipt, err := backend.Apply(context.Background(), root, expectedNewRoot, wl)
-	require.NoError(t, err, "Apply()")
-	require.NotNil(t, mkvsReceipt, "mkvsReceipt")
+	// Apply write log to an empty root.
+	mkvsReceipt, err = backend.Apply(context.Background(), root, expectedNewRoot, wl)
+	require.NoError(t, err, "Apply() should not return an error")
+	require.NotNil(t, mkvsReceipt, "Apply() should return a mkvsReceipt")
 
 	// Check the MKVS receipt and obtain the new root from it.
-	var rb api.MKVSReceiptBody
 	err = mkvsReceipt.Open(&rb)
-	require.NoError(t, err, "mkvsReceipt.Open()")
-	require.Equal(t, uint16(1), rb.Version, "mkvs receipt version")
-	require.Equal(t, 1, len(rb.Roots), "mkvs receipt roots")
-	require.NotEqual(t, root, rb.Roots[0], "mkvs receipt root")
-	require.EqualValues(t, expectedNewRoot, rb.Roots[0], "mkvs receipt root")
+	require.NoError(t, err, "mkvsReceipt.Open() should not return an error")
+	require.Equal(t, uint16(1), rb.Version, "mkvs receipt version should be 1")
+	require.Equal(t, 1, len(rb.Roots), "mkvs receipt should contain 1 root")
+	require.EqualValues(t, expectedNewRoot, rb.Roots[0], "mkvs receipt root should equal the expected new root")
+
+	// Prepare another write log and form a set of apply operations.
+	wl2 := prepareWriteLog(testValues[0:2])
+	expectedNewRoot2 := calculateExpectedNewRoot(t, wl2)
+	applyOps := []api.ApplyOp{
+		api.ApplyOp{Root: root, ExpectedNewRoot: expectedNewRoot, WriteLog: wl},
+		api.ApplyOp{Root: root, ExpectedNewRoot: expectedNewRoot2, WriteLog: wl2},
+	}
+
+	// Apply a batch of operations against the MKVS.
+	mkvsReceipt, err = backend.ApplyBatch(context.Background(), applyOps)
+	require.NoError(t, err, "ApplyBatch() should not return an error")
+	require.NotNil(t, mkvsReceipt, "ApplyBatch() should return a mkvsReceipt")
+
+	// Check the MKVS receipt and obtain the new root from it.
+	err = mkvsReceipt.Open(&rb)
+	require.NoError(t, err, "mkvsReceipt.Open() should not return an error")
+	require.Equal(t, uint16(1), rb.Version, "mkvs receipt version should be 1")
+	require.Equal(t, len(applyOps), len(rb.Roots), "mkvs receipt should contain as many roots as there were applyOps")
+	for i, applyOp := range applyOps {
+		require.EqualValues(t, applyOp.ExpectedNewRoot, rb.Roots[i], "mkvs receipt root for an applyOp should equal the expected new root")
+	}
 
 	var emptyPath hash.Hash
 
@@ -62,6 +104,15 @@ func StorageImplementationTests(t *testing.T, backend api.Backend) {
 	n, err := backend.GetNode(context.Background(), rb.Roots[0], api.NodeID{Path: emptyPath, Depth: 0})
 	require.NoError(t, err, "GetNode()")
 	require.NotNil(t, n)
+
+	// Get the value using its hash.
+	origValue := testValues[2]
+	var hash hash.Hash
+	hash.FromBytes(origValue)
+	value, err := backend.GetValue(context.Background(), rb.Roots[0], hash)
+	require.NoError(t, err, "GetValue() should not return an error")
+	require.NotNil(t, value, "GetValue() should return a value")
+	require.Equal(t, value, origValue, "GetValue()'s returned value should equal the original value")
 
 	// Now try applying the same operations again, we should get the same root.
 	mkvsReceipt, err = backend.Apply(context.Background(), root, rb.Roots[0], wl)
