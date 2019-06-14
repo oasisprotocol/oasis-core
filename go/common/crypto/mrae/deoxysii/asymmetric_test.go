f3993531a6549ec2f0098de1abba639e6ae50e93
Yawning Angel <yawning@schwanenlied.me>
2019-06-14 10:39:14 +0000

    go/common/crypto/mrae/deoxysii: Rename a test

--- go/common/crypto/mrae/deoxysii/asymmetric_test.go
+++ go/common/crypto/mrae/deoxysii/asymmetric_test.go
@@ -7,6 +7,6 @@ import (
 	"github.com/oasislabs/ekiden/go/common/crypto/mrae/api"
 )
 
-func TestSIV_AES_SHA2_Box_Integration(t *testing.T) {
+func TestDeoxysII_Box_Integration(t *testing.T) {
 	api.TestBoxIntegration(t, Box, deoxysii.New, deoxysii.KeySize)
 }
