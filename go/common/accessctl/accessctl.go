d1576b88efedb77662773f2100b1428e3adfc33d
Tadej Janež <tadej.j@nez.si>
2019-06-27 18:25:06 +0200

    Add accessctl common Go package implementing access control

---
+++ go/common/accessctl/accessctl.go
@@ -0,0 +1,58 @@
+// Package accessctl implements access control for Ekiden.
+package accessctl
+
+import (
+	"crypto/x509"
+
+	"github.com/oasislabs/ekiden/go/common/crypto/hash"
+)
+
+// Subject is an access control subject.
+type Subject string
+
+// SubjectFromPublicKey returns a Subject from the given X.509 certificate.
+// To do so, it computes the hash of the certificate's ASN.1 DER representation.
+func SubjectFromCertificate(cert *x509.Certificate) Subject {
+	var h = hash.Hash{}
+	h.FromBytes(cert.Raw)
+	return Subject(h.String())
+}
+
+// Action is an access control action.
+type Action string
+
+// Policy maps from Actions to a mapping from Subjects to booleans indicating
+// whether the given subject is allowed to perform the given action or not.
+type Policy map[Action]map[Subject]bool
+
+// NewPolicy returns an empty policy.
+func NewPolicy() Policy {
+	return make(Policy)
+}
+
+// Allow adds a policy rule that allows the given Subject to perform the given
+// Action.
+func (p Policy) Allow(sub Subject, act Action) {
+	if p[act] == nil {
+		p[act] = make(map[Subject]bool)
+	}
+	p[act][sub] = true
+}
+
+// Deny removes a policy rule that allows the given Subject to perform the
+// given Action.
+func (p Policy) Deny(sub Subject, act Action) {
+	if p[act] == nil {
+		return
+	}
+	delete(p[act], sub)
+}
+
+// IsAllowed returns a boolean indicating whether the given Subject is allowed
+// to perform the given Action under the current Policy.
+func (p Policy) IsAllowed(sub Subject, act Action) bool {
+	if p[act] == nil {
+		return false
+	}
+	return p[act][sub]
+}
