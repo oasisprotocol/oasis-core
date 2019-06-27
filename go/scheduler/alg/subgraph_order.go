1b6889196e792553fa4b4cda9bfa4e151090e6cd
Jernej Kos <jernej@kos.mx>
2019-06-27 12:26:50 +0200

    go/scheduler/alg: Remove txn scheduler algorithm model
    When we need it we can always retrieve it from Git history but it
    currently adds a lot of unnecessary output to tests and may be confusing
    for anyone doing code audits (since it is not actually used anywhere).

--- go/scheduler/alg/subgraph_order.go
+++
@@ -1,8 +0,0 @@
-package alg
-
-// subgraphOrder helper class for sorting subgraphs, highest cost first.
-type subgraphOrder []*Subgraph
-
-func (a subgraphOrder) Len() int           { return len(a) }
-func (a subgraphOrder) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a subgraphOrder) Less(i, j int) bool { return a[i].EstExecutionTime() > a[j].EstExecutionTime() }
