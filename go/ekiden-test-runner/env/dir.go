63de90e573a9fc911978d0bad30703b2e7113a18
Peter Gilbert <pgilbert@oasislabs.com>
2019-10-07 16:46:57 +0000

    ekiden-test-runner: add option to not create temp dir

--- go/ekiden-test-runner/env/dir.go
+++ go/ekiden-test-runner/env/dir.go
@@ -18,6 +18,7 @@ import (
 const (
 	cfgBaseDir          = "basedir"
 	cfgBaseDirNoCleanup = "basedir.no_cleanup"
+	cfgBaseDirNoTempDir = "basedir.no_temp_dir"
 )
 
 var (
@@ -47,12 +48,17 @@ func (d *Dir) Init(cmd *cobra.Command) error {
 	d.dir = viper.GetString(cfgBaseDir)
 	d.noCleanup = viper.GetBool(cfgBaseDirNoCleanup)
 
-	// Create a temporary directory using a prefix derived from the
-	// command's `Use` field.
-	var err error
-	splitUse := strings.Split(cmd.Use, " ")
-	if d.dir, err = ioutil.TempDir(d.dir, splitUse[0]); err != nil {
-		return errors.Wrap(err, "env: failed to create default base directory")
+	if viper.GetBool(cfgBaseDirNoTempDir) {
+		// If we don't create a temporary directory, don't clean up.
+		d.noCleanup = true
+	} else {
+		// Create a temporary directory using a prefix derived from the
+		// command's `Use` field.
+		var err error
+		splitUse := strings.Split(cmd.Use, " ")
+		if d.dir, err = ioutil.TempDir(d.dir, splitUse[0]); err != nil {
+			return errors.Wrap(err, "env: failed to create default base directory")
+		}
 	}
 
 	return nil
@@ -110,6 +116,7 @@ func GetRootDir() *Dir {
 func init() {
 	Flags.String(cfgBaseDir, "", "test base directory")
 	Flags.Bool(cfgBaseDirNoCleanup, false, "do not cleanup test base directory")
+	Flags.Bool(cfgBaseDirNoTempDir, false, "do not create a temp directory inside base directory")
 
 	_ = viper.BindPFlags(Flags)
 }
