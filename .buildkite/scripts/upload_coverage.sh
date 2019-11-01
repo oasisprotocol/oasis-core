3b8085ab3a252cba3f6595a70b8dd63ef2e8bc28
Warren He <w@oasislabs.com>
2019-11-01 14:03:46 -0700

    ci: merge coverage manually

--- .buildkite/scripts/upload_coverage.sh
+++ .buildkite/scripts/upload_coverage.sh
@@ -6,4 +6,4 @@ set +x
 CODECOV_TOKEN=$(cat ~/.codecov/oasis_core_api_token)
 export CODECOV_TOKEN
 set -x
-bash <(curl -s https://codecov.io/bash) -Z
+bash <(curl -s https://codecov.io/bash) -Z -f merged-coverage.txt
