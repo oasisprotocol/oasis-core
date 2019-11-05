fc74a441ec80b6f803d7d2757c5704b20ee386e9
Warren He <w@oasislabs.com>
2019-11-04 17:01:07 -0800

    go oasis-node: add -integration.run explanation

--- go/oasis-node/integrationrunner/integration_test.go
+++ go/oasis-node/integrationrunner/integration_test.go
@@ -9,6 +9,8 @@ var run = flag.Bool("integration.run", false, "Run the node")
 
 func TestIntegration(t *testing.T) {
 	if !*run {
+		// This test requires a bunch of extra arguments and isn't automated on its own. Because of that, it's disabled
+		// by default. This way, running `go test` with ./... works with no fuss.
 		t.Skip("Pass -integration.run to run the node")
 		return
 	}
