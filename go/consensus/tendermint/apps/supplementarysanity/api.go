91af13a9489f0d7da0b7d13281cf5cf31b74ceab
Warren He <w@oasislabs.com>
2019-12-16 13:29:05 -0800

    go supplementarysanity: comment IDs

--- go/consensus/tendermint/apps/supplementarysanity/api.go
+++ go/consensus/tendermint/apps/supplementarysanity/api.go
@@ -1,6 +1,11 @@
 package supplementarysanity
 
 const (
-	AppID   uint8  = 0x98
+	// AppID is the unique application identifier.
+	// This application isn't enabled in the production configuration,
+	// so no need to reserve a low sequential identifier.
+	AppID uint8 = 0x98
+
+	// AppName is the ABCI application name.
 	AppName string = "999_supplementarysanity"
 )
