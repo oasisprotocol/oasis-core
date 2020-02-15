331dffbdbd3ff64e6077cdb5437938419a4d87f7
Warren He <w@oasislabs.com>
2020-02-14 18:06:08 -0800

    ci: add helper to detect only-docs changes

---
+++ .buildkite/scripts/skip_if_only_docs_changes.sh
@@ -0,0 +1,17 @@
+# Source this file rather than fork-exec-ing it.
+
+only_docs_changes() {
+  test -n "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" &&
+    git diff --quiet "refs/remotes/origin/$BUILDKITE_PULL_REQUEST_BASE_BRANCH.." -- \
+      ':(exclude).changelog' \
+      ':(exclude)CHANGELOG.md' \
+      ':(exclude)CONTRIBUTING.md' \
+      ':(exclude)LICENSE' \
+      ':(exclude)README.md' \
+      ':(exclude)SECURITY.md'
+}
+
+if only_docs_changes; then
+  echo "Only docs changes. Skipping"
+  exit 0
+fi
