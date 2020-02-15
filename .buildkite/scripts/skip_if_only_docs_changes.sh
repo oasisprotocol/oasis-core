033c562f1155ca120c8dc7bf2f19dd6f812dece4
Warren He <w@oasislabs.com>
2020-02-14 18:21:18 -0800

    oh our common.sh is all bashed up

--- .buildkite/scripts/skip_if_only_docs_changes.sh
+++ .buildkite/scripts/skip_if_only_docs_changes.sh
@@ -1,6 +1,15 @@
 # Source this file rather than fork-exec-ing it.
 
-. .buildkite/scripts/common.sh
+only_docs_changes() {
+    test -n "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" && \
+        git diff --quiet "refs/remotes/origin/$BUILDKITE_PULL_REQUEST_BASE_BRANCH.." -- \
+            ':(exclude).changelog' \
+            ':(exclude)CHANGELOG.md' \
+            ':(exclude)CONTRIBUTING.md' \
+            ':(exclude)LICENSE' \
+            ':(exclude)README.md' \
+            ':(exclude)SECURITY.md'
+}
 
 if only_docs_changes; then
   echo "Only docs changes. Skipping"
