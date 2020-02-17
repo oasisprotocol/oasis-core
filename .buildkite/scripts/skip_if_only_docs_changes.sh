a0347e67b7ace6ca0fad72eaa881f099886e246f
Warren He <w@oasislabs.com>
2020-02-17 15:21:52 -0800

    ci: broaden docs matching

--- .buildkite/scripts/skip_if_only_docs_changes.sh
+++ .buildkite/scripts/skip_if_only_docs_changes.sh
@@ -4,12 +4,15 @@ only_docs_changes() {
   # https://git-scm.com/docs/gitglossary#Documentation/gitglossary.txt-aiddefpathspecapathspec
   test -n "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" &&
     git diff --quiet "refs/remotes/origin/$BUILDKITE_PULL_REQUEST_BASE_BRANCH.." -- \
-      ':(exclude).changelog' \
-      ':(exclude)CHANGELOG.md' \
-      ':(exclude)CONTRIBUTING.md' \
-      ':(exclude)LICENSE' \
-      ':(exclude)README.md' \
-      ':(exclude)SECURITY.md'
+      ':(exclude)*.md' \
+      ':(exclude).changelog/' \
+      ':(exclude).github/CODEOWNERS' \
+      ':(exclude).github/ISSUE_TEMPLATE/' \
+      ':(exclude).gitlint' \
+      ':(exclude).markdownlint.yml' \
+      ':(exclude).punch_config.py' \
+      ':(exclude)docs/' \
+      ':(exclude)towncrier.toml'
 }
 
 if only_docs_changes; then
