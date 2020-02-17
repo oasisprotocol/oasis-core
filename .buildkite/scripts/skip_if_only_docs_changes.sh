e9e3ee01b190f196fa4607a381d6cf31f6dd0f1d
Warren He <w@oasislabs.com>
2020-02-17 14:55:32 -0800

    ci: add documentation reference

--- .buildkite/scripts/skip_if_only_docs_changes.sh
+++ .buildkite/scripts/skip_if_only_docs_changes.sh
@@ -1,6 +1,7 @@
 # Source this file rather than fork-exec-ing it.
 
 only_docs_changes() {
+  # https://git-scm.com/docs/gitglossary#Documentation/gitglossary.txt-aiddefpathspecapathspec
   test -n "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" &&
     git diff --quiet "refs/remotes/origin/$BUILDKITE_PULL_REQUEST_BASE_BRANCH.." -- \
       ':(exclude).changelog' \
