508193c0ca01f74693548336068aaae93d84bbb3
Warren He <w@oasislabs.com>
2020-02-20 17:03:03 -0800

    ci: merge e2e coverage per job

--- .buildkite/scripts/merge_coverage.sh
+++ .buildkite/scripts/merge_coverage.sh
@@ -4,16 +4,8 @@
 # https://buildkite.com/docs/pipelines/writing-build-scripts
 set -euxo pipefail
 
-# Working directory.
-WORKDIR=$PWD
-
 mkdir -p /tmp/coverage-to-merge
 
 buildkite-agent artifact download "*coverage-*.txt" /tmp/coverage-to-merge
-buildkite-agent artifact download "*coverage-e2e-*.tar.gz" /tmp/coverage-to-merge
 
-shopt -s globstar
-for f in /tmp/**/*.tar.gz; do
-	tar xvzf $f -C /tmp/coverage-to-merge
-done
 gocovmerge /tmp/coverage-to-merge/coverage-*.txt >merged-coverage.txt
