b6fbb5d86b38046225b1d1c0bb0ceba5a1622cbc
ptrus <ptrusr@gmail.com>
2020-06-17 12:36:08 +0200

    go/identity/cli: Add show TLS pubkey commands

--- go/oasis-node/cmd/identity/identity.go
+++ go/oasis-node/cmd/identity/identity.go
@@ -2,6 +2,8 @@
 package identity
 
 import (
+	"crypto/ed25519"
+	"crypto/x509"
 	"fmt"
 	"os"
 
@@ -26,6 +28,18 @@ var (
 		Use:   "init",
 		Short: "initialize node identity",
 		Run:   doNodeInit,
+	}
+
+	identityShowSentryPubkeyCmd = &cobra.Command{
+		Use:   "show-sentry-client-pubkey",
+		Short: "outputs node's sentry control client tls public key",
+		Run:   doShowSentryTLSPubkey,
+	}
+
+	identityShowTLSPubkeyCmd = &cobra.Command{
+		Use:   "show-tls-pubkey",
+		Short: "outputs node's endpoint tls public key",
+		Run:   doShowTLSPubkey,
 	}
 
 	logger = logging.GetLogger("cmd/identity")
@@ -60,12 +74,69 @@ func doNodeInit(cmd *cobra.Command, args []string) {
 	fmt.Printf("Generated identity files in: %s\n", dataDir)
 }
 
+func doShowPubkey(cmd *cobra.Command, args []string, sentry bool) {
+	if err := cmdCommon.Init(); err != nil {
+		cmdCommon.EarlyLogAndExit(err)
+	}
+
+	dataDir := cmdCommon.DataDir()
+	if dataDir == "" {
+		logger.Error("data directory must be set")
+		os.Exit(1)
+	}
+
+	nodeSignerFactory, err := fileSigner.NewFactory(dataDir, signature.SignerNode, signature.SignerP2P, signature.SignerConsensus)
+	if err != nil {
+		logger.Error("failed to create node identity signer factory",
+			"err", err,
+		)
+		os.Exit(1)
+	}
+	identity, err := identity.Load(dataDir, nodeSignerFactory)
+	if err != nil {
+		cmdCommon.EarlyLogAndExit(err)
+	}
+
+	var rawCertificate []byte
+	switch sentry {
+	case true:
+		rawCertificate = identity.TLSSentryClientCertificate.Certificate[0]
+	case false:
+		rawCertificate = identity.GetTLSCertificate().Certificate[0]
+	}
+	cert, err := x509.ParseCertificate(rawCertificate)
+	if err != nil {
+		cmdCommon.EarlyLogAndExit(fmt.Errorf("oasis: failed to parse sentry client certificate: %w", err))
+	}
+	pk, ok := cert.PublicKey.(ed25519.PublicKey)
+	if !ok {
+		cmdCommon.EarlyLogAndExit(fmt.Errorf("oasis: bad sentry client public key type (expected: Ed25519 got: %T)", cert.PublicKey))
+	}
+	var pubKey signature.PublicKey
+	if err := pubKey.UnmarshalBinary(pk[:]); err != nil {
+		cmdCommon.EarlyLogAndExit(fmt.Errorf("oasis: sentry client public key unmarshal failure: %w", err))
+	}
+	key, _ := pubKey.MarshalText()
+
+	fmt.Println(string(key))
+}
+
+func doShowTLSPubkey(cmd *cobra.Command, args []string) {
+	doShowPubkey(cmd, args, false)
+}
+
+func doShowSentryTLSPubkey(cmd *cobra.Command, args []string) {
+	doShowPubkey(cmd, args, true)
+}
+
 // Register registers the client sub-command and all of it's children.
 func Register(parentCmd *cobra.Command) {
 	tendermint.Register(identityCmd)
 
 	identityInitCmd.Flags().AddFlagSet(cmdFlags.VerboseFlags)
 	identityCmd.AddCommand(identityInitCmd)
+	identityCmd.AddCommand(identityShowSentryPubkeyCmd)
+	identityCmd.AddCommand(identityShowTLSPubkeyCmd)
 
 	parentCmd.AddCommand(identityCmd)
 }
