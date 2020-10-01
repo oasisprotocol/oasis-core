f7d756b3cc103d872239d5cfe95efc41f9158dbc
Jernej Kos <jernej@kos.mx>
2020-10-01 11:56:30 +0200

    go/storage: Support node prioritization in read requests

---
+++ go/storage/api/context.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"context"
+
+	"github.com/oasisprotocol/oasis-core/go/common/crypto/signature"
+)
+
+type contextKey string
+
+const contextKeyNodePriorityHint = contextKey("storage/node-priority-key")
+
+// WithNodePriorityHint sets a storage node priority hint for any storage read requests using this
+// context. Only storage nodes that overlap with the configured committee will be used.
+func WithNodePriorityHint(ctx context.Context, nodes []signature.PublicKey) context.Context {
+	return context.WithValue(ctx, contextKeyNodePriorityHint, nodes)
+}
+
+// WithNodePriorityHintFromSignatures sets a storage node priority hint for any storage read
+// requests using this context. Only storage nodes that overlap with the configured committee will
+// be used.
+func WithNodePriorityHintFromSignatures(ctx context.Context, sigs []signature.Signature) context.Context {
+	var nodes []signature.PublicKey
+	for _, s := range sigs {
+		nodes = append(nodes, s.PublicKey)
+	}
+	return WithNodePriorityHint(ctx, nodes)
+}
+
+// NodePriorityHintFromContext returns the storage node priority hint or nil if none is set.
+func NodePriorityHintFromContext(ctx context.Context) []signature.PublicKey {
+	nodes, _ := ctx.Value(contextKeyNodePriorityHint).([]signature.PublicKey)
+	return nodes
+}
