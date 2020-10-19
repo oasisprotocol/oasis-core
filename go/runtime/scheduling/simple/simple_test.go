61fb04bac154769bb58db2d1782701303700b06d
ptrus <ptrusr@gmail.com>
2020-10-19 15:10:16 +0200

    go/runtime/scheduling/simple: Use general txpool interface

--- go/runtime/scheduling/simple/simple_test.go
+++ go/runtime/scheduling/simple/simple_test.go
@@ -6,6 +6,9 @@ import (
 	"github.com/stretchr/testify/require"
 
 	registry "github.com/oasisprotocol/oasis-core/go/registry/api"
+	mapp "github.com/oasisprotocol/oasis-core/go/runtime/scheduling/simple/txpool/map"
+	"github.com/oasisprotocol/oasis-core/go/runtime/scheduling/simple/txpool/orderedmap"
+	"github.com/oasisprotocol/oasis-core/go/runtime/scheduling/simple/txpool/queue"
 	"github.com/oasisprotocol/oasis-core/go/runtime/scheduling/tests"
 )
 
@@ -19,4 +22,37 @@ func TestSimpleScheduler(t *testing.T) {
 	require.NoError(t, err, "New()")
 
 	tests.SchedulerImplementationTests(t, algo)
+}
+
+func BenchmarkSimpleSchedulerQueue(b *testing.B) {
+	params := registry.TxnSchedulerParameters{
+		Algorithm:         Name,
+		MaxBatchSize:      100,
+		MaxBatchSizeBytes: 16 * 1024 * 1024,
+	}
+	algo, err := New(queue.Name, 1000000, params)
+	require.NoError(b, err, "New()")
+	tests.SchedulerImplementationBenchmarks(b, algo)
+}
+
+func BenchmarkSimpleSchedulerMap(b *testing.B) {
+	params := registry.TxnSchedulerParameters{
+		Algorithm:         Name,
+		MaxBatchSize:      100,
+		MaxBatchSizeBytes: 16 * 1024 * 1024,
+	}
+	algo, err := New(mapp.Name, 1000000, params)
+	require.NoError(b, err, "New()")
+	tests.SchedulerImplementationBenchmarks(b, algo)
+}
+
+func BenchmarkSimpleSchedulerOrderedMap(b *testing.B) {
+	params := registry.TxnSchedulerParameters{
+		Algorithm:         Name,
+		MaxBatchSize:      100,
+		MaxBatchSizeBytes: 16 * 1024 * 1024,
+	}
+	algo, err := New(orderedmap.Name, 1000000, params)
+	require.NoError(b, err, "New()")
+	tests.SchedulerImplementationBenchmarks(b, algo)
 }
