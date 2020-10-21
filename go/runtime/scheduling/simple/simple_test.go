63fbd097995c4f5c01a24d5b27d135267c6d06d9
ptrus <ptrusr@gmail.com>
2020-10-21 10:20:02 +0200

    go/runtime/scheduling: Remove unused txpool implementations

--- go/runtime/scheduling/simple/simple_test.go
+++ go/runtime/scheduling/simple/simple_test.go
@@ -6,9 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	registry "github.com/oasisprotocol/oasis-core/go/registry/api"
-	mapp "github.com/oasisprotocol/oasis-core/go/runtime/scheduling/simple/txpool/map"
 	"github.com/oasisprotocol/oasis-core/go/runtime/scheduling/simple/txpool/orderedmap"
-	"github.com/oasisprotocol/oasis-core/go/runtime/scheduling/simple/txpool/queue"
 	"github.com/oasisprotocol/oasis-core/go/runtime/scheduling/tests"
 )
 
@@ -18,32 +16,10 @@ func TestSimpleScheduler(t *testing.T) {
 		MaxBatchSize:      10,
 		MaxBatchSizeBytes: 16 * 1024 * 1024,
 	}
-	algo, err := New(queue.Name, 100, params)
+	algo, err := New(orderedmap.Name, 100, params)
 	require.NoError(t, err, "New()")
 
 	tests.SchedulerImplementationTests(t, algo)
-}
-
-func BenchmarkSimpleSchedulerQueue(b *testing.B) {
-	params := registry.TxnSchedulerParameters{
-		Algorithm:         Name,
-		MaxBatchSize:      100,
-		MaxBatchSizeBytes: 16 * 1024 * 1024,
-	}
-	algo, err := New(queue.Name, 1000000, params)
-	require.NoError(b, err, "New()")
-	tests.SchedulerImplementationBenchmarks(b, algo)
-}
-
-func BenchmarkSimpleSchedulerMap(b *testing.B) {
-	params := registry.TxnSchedulerParameters{
-		Algorithm:         Name,
-		MaxBatchSize:      100,
-		MaxBatchSizeBytes: 16 * 1024 * 1024,
-	}
-	algo, err := New(mapp.Name, 1000000, params)
-	require.NoError(b, err, "New()")
-	tests.SchedulerImplementationBenchmarks(b, algo)
 }
 
 func BenchmarkSimpleSchedulerOrderedMap(b *testing.B) {
