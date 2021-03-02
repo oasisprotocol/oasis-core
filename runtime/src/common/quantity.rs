9ef0e5b0c6908c638f1c3b98a910ca5b4c198fce
Jernej Kos <jernej@kos.mx>
2021-03-02 12:46:40 +0100

    runtime/common/quantity: Add Mul/Div implementations

--- runtime/src/common/quantity.rs
+++ runtime/src/common/quantity.rs
@@ -2,11 +2,11 @@
 use core::cmp::Ordering::{Equal, Greater, Less};
 use std::{
     fmt,
-    ops::{Add, AddAssign, Sub},
+    ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub},
 };
 
 use num_bigint::BigUint;
-use num_traits::{Num, Zero};
+use num_traits::{CheckedDiv, CheckedSub, Num, Zero};
 use serde;
 
 /// An arbitrary precision unsigned integer.
@@ -19,11 +19,16 @@ impl Quantity {
     pub fn checked_sub(&self, other: &Quantity) -> Option<Quantity> {
         // NOTE: This does not implemented the num_traits::CheckedSub trait because this forces
         //       one to also implement Sub which we explicitly don't want to do.
-        match self.0.cmp(&other.0) {
-            Less => None,
-            Equal => Some(Zero::zero()),
-            Greater => Some(Quantity(self.0.clone().sub(&other.0))),
-        }
+        self.0.checked_sub(&other.0).map(Quantity)
+    }
+
+    /// Divides two numbers, checking for underflow, overflow and division by zero. If any of that
+    /// happens, `None` is returned.
+    #[inline]
+    pub fn checked_div(&self, other: &Quantity) -> Option<Quantity> {
+        // NOTE: This does not implemented the num_traits::CheckedDiv trait because this forces
+        //       one to also implement Div which we explicitly don't want to do.
+        self.0.checked_div(&other.0).map(Quantity)
     }
 }
 
@@ -64,6 +69,72 @@ impl<'a> Add<&'a Quantity> for Quantity {
 impl<'a> AddAssign<&'a Quantity> for Quantity {
     fn add_assign(&mut self, other: &Quantity) {
         self.0 += &other.0;
+    }
+}
+
+impl AddAssign<Quantity> for Quantity {
+    fn add_assign(&mut self, other: Quantity) {
+        self.0 += other.0;
+    }
+}
+
+impl Add<u64> for Quantity {
+    type Output = Quantity;
+
+    fn add(mut self, other: u64) -> Quantity {
+        self += other;
+        self
+    }
+}
+
+impl AddAssign<u64> for Quantity {
+    fn add_assign(&mut self, other: u64) {
+        self.0 += other;
+    }
+}
+
+impl Mul for Quantity {
+    type Output = Quantity;
+
+    fn mul(mut self, rhs: Quantity) -> Quantity {
+        self *= &rhs;
+        self
+    }
+}
+
+impl<'a> Mul<&'a Quantity> for Quantity {
+    type Output = Quantity;
+
+    fn mul(mut self, rhs: &Quantity) -> Quantity {
+        self *= rhs;
+        self
+    }
+}
+
+impl<'a> MulAssign<&'a Quantity> for Quantity {
+    fn mul_assign(&mut self, rhs: &Quantity) {
+        self.0 *= &rhs.0;
+    }
+}
+
+impl MulAssign<Quantity> for Quantity {
+    fn mul_assign(&mut self, rhs: Quantity) {
+        self.0 *= rhs.0;
+    }
+}
+
+impl Mul<u64> for Quantity {
+    type Output = Quantity;
+
+    fn mul(mut self, other: u64) -> Quantity {
+        self *= other;
+        self
+    }
+}
+
+impl MulAssign<u64> for Quantity {
+    fn mul_assign(&mut self, other: u64) {
+        self.0 *= other;
     }
 }
 
@@ -159,5 +230,57 @@ mod test {
             let dec: Quantity = cbor::from_slice(&enc).expect("deserialization should succeed");
             assert_eq!(dec, q, "serialization should round-trip");
         }
+    }
+
+    #[test]
+    fn test_ops() {
+        // Add.
+        assert_eq!(
+            Quantity::from(1000) + Quantity::from(2000),
+            Quantity::from(3000)
+        );
+
+        let mut a = Quantity::from(1000);
+        a += Quantity::from(42);
+        assert_eq!(a, Quantity::from(1042));
+        a += &Quantity::from(42);
+        assert_eq!(a, Quantity::from(1084));
+
+        let mut a = Quantity::from(1000);
+        a += 42;
+        assert_eq!(a, Quantity::from(1042));
+
+        // Sub.
+        let a = Quantity::from(1000);
+        assert_eq!(
+            a.checked_sub(&Quantity::from(42)),
+            Some(Quantity::from(958))
+        );
+        assert_eq!(a.checked_sub(&Quantity::from(1100)), None);
+
+        // Mul.
+        assert_eq!(
+            Quantity::from(1000) * Quantity::from(1000),
+            Quantity::from(1_000_000)
+        );
+
+        let mut a = Quantity::from(1000);
+        a *= Quantity::from(1000);
+        assert_eq!(a, Quantity::from(1_000_000));
+        a *= &Quantity::from(1000);
+        assert_eq!(a, Quantity::from(1_000_000_000));
+
+        let mut a = Quantity::from(1000);
+        a *= 1000;
+        assert_eq!(a, Quantity::from(1_000_000));
+
+        // Div.
+        let a = Quantity::from(1000);
+        assert_eq!(a.checked_div(&Quantity::from(3)), Some(Quantity::from(333)));
+        assert_eq!(
+            a.checked_div(&Quantity::from(1001)),
+            Some(Quantity::from(0))
+        );
+        assert_eq!(a.checked_div(&Quantity::from(0)), None);
     }
 }
