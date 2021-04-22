dec953c6da71424f54deb5eae859f0b37e7e8486
Jernej Kos <jernej@kos.mx>
2021-04-22 15:00:12 +0200

    go/oasis-node: Dump correct block height on halt

--- go/oasis-node/cmd/node/node.go
+++ go/oasis-node/cmd/node/node.go
@@ -205,9 +205,9 @@ func (n *Node) startRuntimeServices() error {
 	n.Consensus.RegisterHaltHook(func(ctx context.Context, blockHeight int64, epoch epochtime.EpochTime, _ error) {
 		n.logger.Info("Consensus halt hook: dumping genesis",
 			"epoch", epoch,
-			"block_height", blockHeight,
+			"block_height", blockHeight+1,
 		)
-		if err = n.dumpGenesis(ctx, blockHeight, epoch); err != nil {
+		if err = n.dumpGenesis(ctx, blockHeight+1, epoch); err != nil {
 			n.logger.Error("halt hook: failed to dump genesis",
 				"err", err,
 			)
@@ -215,7 +215,7 @@ func (n *Node) startRuntimeServices() error {
 		}
 		n.logger.Info("Consensus halt hook: genesis dumped",
 			"epoch", epoch,
-			"block_height", blockHeight,
+			"block_height", blockHeight+1,
 		)
 	})
 
