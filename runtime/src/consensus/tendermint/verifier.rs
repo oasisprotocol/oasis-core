63257ca9878e2c8e270d00858fff6f1f372bedee
ptrus <peter@u-s.si>
2022-02-16 10:44:44 +0100

    tendermint-rs: bump to 0.23.5

--- runtime/src/consensus/tendermint/verifier.rs
+++ runtime/src/consensus/tendermint/verifier.rs
@@ -21,13 +21,14 @@ use tendermint_light_client::{
     components::{self, verifier::PredicateVerifier},
     light_client,
     operations::{ProdCommitValidator, ProdHasher, VotingPowerCalculator, VotingPowerTally},
-    predicates::{errors::VerificationError, ProdPredicates},
+    predicates::ProdPredicates,
     supervisor::Instance,
     types::{
         Commit, Hash as TMHash, LightBlock as TMLightBlock, PeerId, SignedHeader, Time,
         TrustThreshold, ValidatorSet,
     },
 };
+use tendermint_light_client_verifier::errors::VerificationError;
 use tendermint_rpc::error::Error as RpcError;
 
 use super::store::LruStore;
@@ -590,7 +591,7 @@ struct InsecureClock;
 
 impl components::clock::Clock for InsecureClock {
     fn now(&self) -> Time {
-        Time(time::insecure_posix_system_time().into())
+        Time::from_unix_timestamp(time::insecure_posix_time(), 0).unwrap()
     }
 }
 
