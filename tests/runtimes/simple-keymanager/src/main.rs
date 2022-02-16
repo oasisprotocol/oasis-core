45354d8934172228026d03a48319f653e4d5ffb1
Yawning Angel <yawning@schwanenlied.me>
2022-02-16 13:13:50 +0000

    tests: Actually alter the runtime version in the upgraded builds

--- tests/runtimes/simple-keymanager/src/main.rs
+++ tests/runtimes/simple-keymanager/src/main.rs
@@ -1,15 +1,24 @@
 use oasis_core_keymanager_lib::keymanager::*;
-use oasis_core_runtime::{common::version::Version, config::Config, version_from_cargo};
+use oasis_core_runtime::{common::version::Version, config::Config};
 
 mod api;
 
-pub fn main() {
+pub fn main_with_version(version: Version) {
     let init = new_keymanager(api::trusted_policy_signers());
     oasis_core_runtime::start_runtime(
         init,
         Config {
-            version: version_from_cargo!(),
+            version,
             ..Default::default()
         },
     );
+}
+
+#[allow(dead_code)]
+pub fn main() {
+    main_with_version(Version {
+        major: 0,
+        minor: 0,
+        patch: 0,
+    })
 }
