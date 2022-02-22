67d34ff9b97ad0eb1282bdebc6d68bef609565fe
Jernej Kos <jernej@kos.mx>
2022-02-22 11:51:42 +0100

    tests/runtimes/simple-keymanager: Remove OASIS_UNSAFE_KM_POLICY_KEYS
    There is no reason to have it as this keymanager implementation is only
    for use in tests and will never have a production policy.

--- tests/runtimes/simple-keymanager/src/api.rs
+++ tests/runtimes/simple-keymanager/src/api.rs
@@ -6,22 +6,18 @@ pub fn trusted_policy_signers() -> TrustedPolicySigners {
     TrustedPolicySigners {
         signers: {
             let mut set = HashSet::new();
-            if option_env!("OASIS_UNSAFE_KM_POLICY_KEYS").is_some() {
-                for seed in [
-                    "ekiden key manager test multisig key 0",
-                    "ekiden key manager test multisig key 1",
-                    "ekiden key manager test multisig key 2",
-                ]
-                .iter()
-                {
-                    let private_key = OasisPrivateKey::from_test_seed(seed.to_string());
-                    set.insert(private_key.public_key());
-                }
+            for seed in [
+                "ekiden key manager test multisig key 0",
+                "ekiden key manager test multisig key 1",
+                "ekiden key manager test multisig key 2",
+            ]
+            .iter()
+            {
+                let private_key = OasisPrivateKey::from_test_seed(seed.to_string());
+                set.insert(private_key.public_key());
             }
-
-            // TODO: Populate with the production keys as well.
             set
         },
-        threshold: 9001, // TODO: Set this to a real value.
+        threshold: 2,
     }
 }
