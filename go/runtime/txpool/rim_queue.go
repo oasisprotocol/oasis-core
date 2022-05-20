6b7e5293683c4d00750449e708f703bd1b012afc
Warren He <w@oasislabs.com>
2022-05-19 17:05:51 -0700

    go/runtime/txpool: new queue structures

---
+++ go/runtime/txpool/rim_queue.go
@@ -0,0 +1,27 @@
+package txpool
+
+import "github.com/oasisprotocol/oasis-core/go/common/crypto/hash"
+
+var _ UsableTransactionSource = (*rimQueue)(nil)
+
+// rimQueue exposes transactions form roothash incoming messages.
+type rimQueue struct{}
+
+func newRimQueue() *rimQueue {
+	return &rimQueue{}
+}
+
+func (rq *rimQueue) GetSchedulingSuggestion(countHint uint32) []*TxQueueMeta {
+	// Runtimes instead get transactions from the incoming messages.
+	return nil
+}
+
+func (rq *rimQueue) GetTxByHash(h hash.Hash) *TxQueueMeta {
+	// TODO implement me
+	panic("implement me")
+	// get incoming messages, parse them, extract txs, hash them, look up by hash here
+}
+
+func (rq *rimQueue) HandleTxsUsed(hashes []hash.Hash) {
+	// The roothash module manages the incoming message queue on its own, so we don't do anything here.
+}
