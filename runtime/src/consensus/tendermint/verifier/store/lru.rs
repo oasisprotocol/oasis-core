5b0367147c71f9c70e43aad40b4e0636939b81a0
Jernej Kos <jernej@kos.mx>
2023-01-10 14:18:36 +0100

    runtime: Fix verification of blocks between two trusted heights

--- runtime/src/consensus/tendermint/verifier/store/lru.rs
+++ runtime/src/consensus/tendermint/verifier/store/lru.rs
@@ -20,15 +20,29 @@ impl std::fmt::Debug for LruStore {
     }
 }
 
+struct SubStore {
+    lowest_height: Option<Height>,
+    blocks: lru::LruCache<Height, LightBlock>,
+}
+
+impl SubStore {
+    fn new(capacity: usize) -> Self {
+        Self {
+            lowest_height: None,
+            blocks: lru::LruCache::new(NonZeroUsize::new(capacity).unwrap()),
+        }
+    }
+}
+
 struct Inner {
-    unverified: lru::LruCache<Height, LightBlock>,
-    verified: lru::LruCache<Height, LightBlock>,
-    trusted: lru::LruCache<Height, LightBlock>,
-    failed: lru::LruCache<Height, LightBlock>,
+    unverified: SubStore,
+    verified: SubStore,
+    trusted: SubStore,
+    failed: SubStore,
 }
 
 impl Inner {
-    fn store(&mut self, status: Status) -> &mut lru::LruCache<Height, LightBlock> {
+    fn store(&mut self, status: Status) -> &mut SubStore {
         match status {
             Status::Unverified => &mut self.unverified,
             Status::Verified => &mut self.verified,
@@ -43,10 +57,10 @@ impl LruStore {
     pub fn new(capacity: usize) -> Self {
         Self {
             inner: Mutex::new(Inner {
-                unverified: lru::LruCache::new(NonZeroUsize::new(capacity).unwrap()),
-                verified: lru::LruCache::new(NonZeroUsize::new(capacity).unwrap()),
-                trusted: lru::LruCache::new(NonZeroUsize::new(capacity).unwrap()),
-                failed: lru::LruCache::new(NonZeroUsize::new(capacity).unwrap()),
+                unverified: SubStore::new(capacity),
+                verified: SubStore::new(capacity),
+                trusted: SubStore::new(capacity),
+                failed: SubStore::new(capacity),
             }),
         }
     }
@@ -62,39 +76,68 @@ impl LruStore {
 
 impl LightStore for LruStore {
     fn get(&self, height: Height, status: Status) -> Option<LightBlock> {
-        self.inner().store(status).get(&height).cloned()
+        self.inner().store(status).blocks.get(&height).cloned()
     }
 
     fn insert(&mut self, light_block: LightBlock, status: Status) {
-        self.inner_mut()
-            .store(status)
-            .put(light_block.height(), light_block);
+        let mut store = self.inner_mut().store(status);
+
+        // Promote lowest to prevent it from being evicted.
+        let height = light_block.height();
+        if let Some(lowest_height) = store.lowest_height {
+            if height < lowest_height {
+                store.lowest_height = Some(height);
+            } else {
+                store.blocks.promote(&lowest_height);
+            }
+        } else {
+            store.lowest_height = Some(height);
+        }
+
+        store.blocks.put(height, light_block);
     }
 
     fn remove(&mut self, height: Height, status: Status) {
-        self.inner_mut().store(status).pop(&height);
+        let mut store = self.inner_mut().store(status);
+
+        store.blocks.pop(&height);
+
+        // Check if this is the lowest height being explicitly removed.
+        if store.lowest_height.map(|lh| height == lh).unwrap_or(false) {
+            store.lowest_height = store.blocks.iter().map(|(height, _)| *height).min();
+        }
     }
 
     fn update(&mut self, light_block: &LightBlock, status: Status) {
-        self.inner_mut()
-            .store(status)
-            .put(light_block.height(), light_block.clone());
+        self.insert(light_block.clone(), status)
     }
 
     fn highest(&self, status: Status) -> Option<LightBlock> {
         self.inner()
             .store(status)
+            .blocks
             .iter()
             .max_by_key(|(&height, _)| height)
             .map(|(_, lb)| lb.clone())
     }
 
-    fn lowest(&self, status: Status) -> Option<LightBlock> {
+    fn highest_before(&self, height: Height, status: Status) -> Option<LightBlock> {
         self.inner()
             .store(status)
+            .blocks
             .iter()
-            .min_by_key(|(&height, _)| height)
+            .filter(|(h, _)| h <= &&height)
+            .max_by_key(|(&height, _)| height)
             .map(|(_, lb)| lb.clone())
+    }
+
+    fn lowest(&self, status: Status) -> Option<LightBlock> {
+        let mut inner = self.inner();
+        let store = inner.store(status);
+
+        store
+            .lowest_height
+            .and_then(|lowest_height| store.blocks.get(&lowest_height).cloned())
     }
 
     #[allow(clippy::needless_collect)]
@@ -102,10 +145,92 @@ impl LightStore for LruStore {
         let light_blocks: Vec<_> = self
             .inner()
             .store(status)
+            .blocks
             .iter()
             .map(|(_, lb)| lb.clone())
             .collect();
 
         Box::new(light_blocks.into_iter())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use tendermint_light_client::{
+        store::LightStore,
+        types::{LightBlock, Status},
+    };
+    use tendermint_testgen::{Generator, LightChain};
+
+    use super::LruStore;
+
+    fn generate_blocks(count: u64) -> Vec<LightBlock> {
+        LightChain::default_with_length(count)
+            .light_blocks
+            .into_iter()
+            .map(|lb| lb.generate().unwrap())
+            .map(|lb| LightBlock {
+                signed_header: lb.signed_header,
+                validators: lb.validators,
+                next_validators: lb.next_validators,
+                provider: lb.provider,
+            })
+            .collect()
+    }
+
+    #[test]
+    fn test_lowest_height_retained() {
+        let blocks = generate_blocks(10);
+        let mut store = LruStore::new(2); // Only storing two blocks.
+        store.insert(blocks[0].clone(), Status::Trusted);
+        store.insert(blocks[1].clone(), Status::Trusted);
+        store.insert(blocks[2].clone(), Status::Trusted);
+        store.insert(blocks[3].clone(), Status::Trusted);
+
+        let lowest = store
+            .lowest(Status::Trusted)
+            .expect("there should be a lowest block");
+        assert_eq!(lowest, blocks[0]);
+    }
+
+    #[test]
+    fn test_basic() {
+        let blocks = generate_blocks(10);
+        let mut store = LruStore::new(10);
+        for block in &blocks {
+            store.insert(block.clone(), Status::Trusted);
+
+            // Block should be stored.
+            let stored_block = store.get(block.height(), Status::Trusted);
+            assert_eq!(stored_block.as_ref(), Some(block));
+
+            // Highest and lowest blocks should be correct.
+            let highest = store
+                .highest(Status::Trusted)
+                .expect("there should be a highest block");
+            let lowest = store
+                .lowest(Status::Trusted)
+                .expect("there should be a lowest block");
+            assert_eq!(&highest, block);
+            assert_eq!(lowest, blocks[0]);
+
+            // Highest before should work.
+            let highest_before = store.highest_before(block.height(), Status::Trusted);
+            assert_eq!(highest_before.as_ref(), Some(block));
+
+            let highest_before = store.highest_before(block.height().increment(), Status::Trusted);
+            assert_eq!(highest_before.as_ref(), Some(block));
+        }
+
+        // Test removal of lowest block.
+        store.remove(blocks[0].height(), Status::Trusted);
+
+        let block_zero = store.get(blocks[0].height(), Status::Trusted);
+        assert!(block_zero.is_none());
+
+        let lowest = store
+            .lowest(Status::Trusted)
+            .expect("there should be a lowest block");
+        assert_eq!(lowest, blocks[1]);
     }
 }
--- runtime/src/consensus/tendermint/verifier/store/state.rs
+++ runtime/src/consensus/tendermint/verifier/store/state.rs
@@ -2,17 +2,17 @@ use io_context::Context;
 use std::sync::Arc;
 
 use sgx_isa::Keypolicy;
-use tendermint_light_client::types::LightBlock as TMLightBlock;
+use tendermint_light_client::{
+    store::LightStore,
+    types::{LightBlock as TMLightBlock, Status},
+};
 
 use crate::{
     common::{
         namespace::Namespace,
         sgx::{seal, EnclaveIdentity},
     },
-    consensus::{
-        tendermint::{encode_light_block, LightBlockMeta},
-        verifier::{Error, TrustRoot, TrustedState},
-    },
+    consensus::verifier::{Error, TrustRoot},
     protocol::ProtocolUntrustedLocalStorage,
     storage::KeyValue,
     Protocol,
@@ -26,6 +26,48 @@ const TRUSTED_STATE_STORAGE_KEY_PREFIX: &str = "tendermint.verifier.trusted_stat
 
 /// Domain separation context for the trusted state.
 const TRUSTED_STATE_CONTEXT: &[u8] = b"oasis-core/verifier: trusted state";
+
+/// An encoded Tendermint light block.
+#[derive(Debug, Clone)]
+pub struct EncodedLightBlock(TMLightBlock);
+
+impl From<TMLightBlock> for EncodedLightBlock {
+    fn from(value: TMLightBlock) -> Self {
+        Self(value)
+    }
+}
+
+impl From<EncodedLightBlock> for TMLightBlock {
+    fn from(value: EncodedLightBlock) -> Self {
+        value.0
+    }
+}
+
+impl cbor::Encode for EncodedLightBlock {
+    fn into_cbor_value(self) -> cbor::Value {
+        cbor::serde::to_value(&self.0).unwrap()
+    }
+}
+
+impl cbor::Decode for EncodedLightBlock {
+    fn try_from_cbor_value(value: cbor::Value) -> Result<Self, cbor::DecodeError> {
+        cbor::serde::from_value(value)
+            .map_err(|_| cbor::DecodeError::ParsingFailed)
+            .map(Self)
+    }
+}
+
+/// Trusted state containing trust root and trusted light block.
+#[derive(Debug, Clone, Default, cbor::Encode, cbor::Decode)]
+pub struct TrustedState {
+    /// Trust root.
+    pub trust_root: TrustRoot,
+    /// Trusted light blocks, ordered by height from lowest to highest.
+    ///
+    /// Optional as we don't want to force trusted state for embedded trust
+    /// root to have a matching trusted light block.
+    pub trusted_blocks: Vec<EncodedLightBlock>,
+}
 
 /// Untrusted local storage for storing the sealed latest trusted root.
 pub struct TrustedStateStore {
@@ -47,22 +89,27 @@ impl TrustedStateStore {
         }
     }
 
-    pub fn save(&self, trusted_block: &TMLightBlock) {
-        // Build trusted state.
+    /// Persist latest trusted state from the in-memory light store.
+    ///
+    /// # Panics
+    ///
+    /// Panics in case the light store does not have any blocks or if insertion to the underlying
+    /// runtime's untrusted local store fails.
+    pub fn save(&self, store: &Box<dyn LightStore>) {
+        let lowest_block = store.lowest(Status::Trusted).unwrap();
+        let highest_block = store.highest(Status::Trusted).unwrap();
+
+        // Generate a new trust root from the highest trusted block.
         let trust_root = TrustRoot {
-            height: trusted_block.height().into(),
-            hash: trusted_block.signed_header.header.hash().to_string(),
+            height: highest_block.height().into(),
+            hash: highest_block.signed_header.header.hash().to_string(),
             runtime_id: self.runtime_id,
             chain_context: self.chain_context.clone(),
         };
-        let lbm = LightBlockMeta {
-            signed_header: Some(trusted_block.signed_header.clone()),
-            validators: trusted_block.validators.clone(),
-        };
-        let trusted_block = Some(encode_light_block(&lbm).unwrap());
+
         let trusted_state = TrustedState {
             trust_root,
-            trusted_block,
+            trusted_blocks: vec![lowest_block.into(), highest_block.into()],
         };
 
         // Serialize and seal the trusted state.
@@ -75,6 +122,9 @@ impl TrustedStateStore {
             .unwrap();
     }
 
+    /// Attempts to load previously sealed trusted state.
+    ///
+    /// If no sealed trusted state is available, it returns state based on the passed trust root.
     pub fn load(&self, trust_root: &TrustRoot) -> Result<TrustedState, Error> {
         // Attempt to load the previously sealed trusted state.
         let untrusted_value = self
@@ -84,7 +134,7 @@ impl TrustedStateStore {
         if untrusted_value.is_empty() {
             return Ok(TrustedState {
                 trust_root: trust_root.clone(),
-                trusted_block: None,
+                trusted_blocks: vec![],
             });
         }
 
