426fc367e18bc159a7f21338815ba3901018dad4
Peter Nose <peter.nose@gmail.com>
2023-11-29 14:59:00 +0100

    go/oasis-node/cmd: Add cmd to sign Eden genesis block

--- go/oasis-node/cmd/consensus/consensus.go
+++ go/oasis-node/cmd/consensus/consensus.go
@@ -3,23 +3,32 @@ package consensus
 
 import (
 	"context"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"os"
+	"time"
 
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"google.golang.org/grpc"
+	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	cmttypes "github.com/cometbft/cometbft/types"
 
 	"github.com/oasisprotocol/oasis-core/go/common/cbor"
+	"github.com/oasisprotocol/oasis-core/go/common/identity"
 	"github.com/oasisprotocol/oasis-core/go/common/logging"
 	"github.com/oasisprotocol/oasis-core/go/common/prettyprint"
 	consensus "github.com/oasisprotocol/oasis-core/go/consensus/api"
 	"github.com/oasisprotocol/oasis-core/go/consensus/api/transaction"
+	abciAPI "github.com/oasisprotocol/oasis-core/go/consensus/cometbft/api"
+	"github.com/oasisprotocol/oasis-core/go/consensus/cometbft/crypto"
+	"github.com/oasisprotocol/oasis-core/go/oasis-node/cmd/common"
 	cmdCommon "github.com/oasisprotocol/oasis-core/go/oasis-node/cmd/common"
 	cmdConsensus "github.com/oasisprotocol/oasis-core/go/oasis-node/cmd/common/consensus"
 	cmdFlags "github.com/oasisprotocol/oasis-core/go/oasis-node/cmd/common/flags"
 	cmdGrpc "github.com/oasisprotocol/oasis-core/go/oasis-node/cmd/common/grpc"
+	cmdSigner "github.com/oasisprotocol/oasis-core/go/oasis-node/cmd/common/signer"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
 )
 
 const (
@@ -61,6 +70,12 @@ var (
 		Use:        "next_block_state",
 		Run:        doNextBlockState,
 		Deprecated: "use the `oasis` CLI instead.",
+	}
+
+	signEdenGenesisBlockCmd = &cobra.Command{
+		Use:   "sign_eden_genesis_block",
+		Short: "sign Eden genesis block",
+		Run:   doSignEdenGenesisBlock,
 	}
 
 	logger = logging.GetLogger("cmd/consensus")
@@ -207,6 +222,68 @@ func doNextBlockState(cmd *cobra.Command, _ []string) {
 	fmt.Println(string(prettyStatus))
 }
 
+func doSignEdenGenesisBlock(cmd *cobra.Command, _ []string) {
+	if err := cmdCommon.Init(); err != nil {
+		cmdCommon.EarlyLogAndExit(err)
+	}
+	dataDir := common.DataDir()
+
+	// Prepare signer.
+	signerFactory, err := cmdSigner.NewFactory(cmdSigner.Backend(), dataDir, identity.RequiredSignerRoles...)
+	if err != nil {
+		cmdCommon.EarlyLogAndExit(err)
+	}
+	identity, err := identity.LoadOrGenerate(dataDir, signerFactory)
+	if err != nil {
+		cmdCommon.EarlyLogAndExit(err)
+	}
+	signer := crypto.SignerToCometBFT(identity.ConsensusSigner)
+
+	// Eden genesis block data
+	chainContext := "bb3d748def55bdfb797a2ac53ee6ee141e54cd2ab2dc2375f4a0703a178e6e55"
+	chainID := abciAPI.CometBFTChainID(chainContext)
+
+	timeString := "2023-11-29 11:25:17.649247857 +0000 UTC"
+	layout := "2006-01-02 15:04:05.999999999 -0700 MST"
+	timestamp, err := time.Parse(layout, timeString)
+	if err != nil {
+		cmdCommon.EarlyLogAndExit(err)
+	}
+
+	blockHash, err := hex.DecodeString("7670c86852c214f4e2b5ad571f4c2c344c630afdb409f7a5a3adce5a85240f31")
+	if err != nil {
+		cmdCommon.EarlyLogAndExit(err)
+	}
+
+	headerHash, err := hex.DecodeString("4d2aeef8f066f67f5324590df7b0f1ca1c5198836743125898887787ed803155")
+	if err != nil {
+		cmdCommon.EarlyLogAndExit(err)
+	}
+
+	// Prepare signature.
+	vote := cmtproto.Vote{
+		Type:   2,
+		Height: 16817956,
+		Round:  2,
+		BlockID: cmtproto.BlockID{
+			Hash: blockHash,
+			PartSetHeader: cmtproto.PartSetHeader{
+				Total: 1,
+				Hash:  headerHash,
+			},
+		},
+		Timestamp: timestamp,
+	}
+
+	signBytes := cmttypes.VoteSignBytes(chainID, &vote)
+	sig, err := signer.Sign(signBytes)
+	if err != nil {
+		cmdCommon.EarlyLogAndExit(err)
+	}
+
+	fmt.Println(hex.EncodeToString(sig))
+}
+
 // Register registers the consensus sub-command and all of it's children.
 func Register(parentCmd *cobra.Command) {
 	for _, v := range []*cobra.Command{
@@ -214,6 +291,7 @@ func Register(parentCmd *cobra.Command) {
 		showTxCmd,
 		estimateGasCmd,
 		nextBlockStateCmd,
+		signEdenGenesisBlockCmd,
 	} {
 		consensusCmd.AddCommand(v)
 	}
