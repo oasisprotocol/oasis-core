b30f0c061899c2f258945b4919dc2b96c18a31b0
Peter Nose <peter.nose@gmail.com>
2024-04-25 13:23:43 +0200

    secret-sharing/src/churp: Encode shareholder ID to prime field

--- secret-sharing/src/churp/suits.rs
+++ secret-sharing/src/churp/suits.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
 
-use group::{ff::PrimeField, Group, GroupEncoding};
+use group::{
+    ff::{Field, PrimeField},
+    Group, GroupEncoding,
+};
 use p384::elliptic_curve::hash2curve::{ExpandMsgXmd, GroupDigest};
 
 use super::{Error, Shareholder};
@@ -8,6 +11,9 @@ use super::{Error, Shareholder};
 /// Domain separation tag for encoding to NIST P-384 prime field or curve
 /// using the SHA3-384 hash function.
 const NIST_P384_SHA3_384_ENC_DST: &[u8] = b"P384_XMD:SHA3-384_SSWU_RO_";
+
+/// Domain separation tag for encoding shareholder identifiers.
+const SHAREHOLDER_ENC_DST: &[u8] = b"shareholder";
 
 /// Cipher suite.
 pub trait Suite {
@@ -23,8 +29,17 @@ pub trait Suite {
     /// Hashes an arbitrary-length byte string to a point on the elliptic curve.
     fn hash_to_curve(msg: &[u8], dst: &[u8]) -> Result<Self::Group>;
 
-    /// Maps given shareholder ID to a non-zero element of the prime field.
-    fn encode_shareholder(id: Shareholder) -> Result<Self::PrimeField>;
+    /// Encodes the given shareholder ID to a non-zero element of the prime field.
+    fn encode_shareholder(id: Shareholder) -> Result<Self::PrimeField> {
+        let s = Self::hash_to_field(&id.0[..], SHAREHOLDER_ENC_DST)
+            .map_err(|_| Error::ShareholderEncodingFailed)?;
+
+        if s.is_zero().into() {
+            return Err(Error::ZeroValueShareholder.into());
+        }
+
+        Ok(s)
+    }
 }
 
 /// The NIST P-384 elliptic curve group with the SHA3-384 hash function used
@@ -50,18 +65,6 @@ impl Suite for NistP384Sha3_384 {
         let p = p384::NistP384::hash_from_bytes::<ExpandMsgXmd<sha3::Sha3_384>>(&msgs, &dsts)?;
         Ok(p)
     }
-
-    fn encode_shareholder(id: Shareholder) -> Result<Self::PrimeField> {
-        let mut bytes = [0u8; 48];
-        bytes[16..].copy_from_slice(&id.0);
-
-        let s = p384::Scalar::from_slice(&bytes).or(Err(Error::ShareholderEncodingFailed))?;
-        if s.is_zero().into() {
-            return Err(Error::ZeroValueShareholder.into());
-        }
-
-        Ok(s)
-    }
 }
 
 #[cfg(test)]
@@ -72,25 +75,7 @@ mod tests {
 
     use rand::{rngs::StdRng, RngCore, SeedableRng};
 
-    use super::{Error, NistP384Sha3_384, Shareholder, Suite};
-
-    #[test]
-    fn test_encode() {
-        let id = [0; 32];
-        let zero = NistP384Sha3_384::encode_shareholder(Shareholder(id));
-        assert!(zero.is_err());
-        assert_eq!(
-            zero.unwrap_err().to_string(),
-            Error::ZeroValueShareholder.to_string()
-        );
-
-        let mut id = [0; 32];
-        id[30] = 3;
-        id[31] = 232;
-        let thousand = NistP384Sha3_384::encode_shareholder(Shareholder(id));
-        assert!(thousand.is_ok());
-        assert_eq!(thousand.unwrap(), p384::Scalar::from_u64(1000));
-    }
+    use super::{NistP384Sha3_384, Shareholder, Suite};
 
     #[bench]
     fn bench_hash_to_field_p384_sha3_384(b: &mut Bencher) {
@@ -111,6 +96,17 @@ mod tests {
         b.iter(|| {
             rng.fill_bytes(&mut data);
             let _ = NistP384Sha3_384::hash_to_curve(&data[..32], &data[32..]).unwrap();
+        });
+    }
+
+    #[bench]
+    fn bench_encode_shareholder_p384_sha3_384(b: &mut Bencher) {
+        let mut rng: StdRng = SeedableRng::from_seed([1u8; 32]);
+        let mut id = [0; 32];
+
+        b.iter(|| {
+            rng.fill_bytes(&mut id);
+            let _ = NistP384Sha3_384::encode_shareholder(Shareholder(id)).unwrap();
         });
     }
 }
