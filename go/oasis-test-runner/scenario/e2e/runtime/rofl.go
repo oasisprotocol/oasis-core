0abfae0f687e9b63ffbe6056f577061f3825acdf
Peter Nose <peter.nose@gmail.com>
2025-01-27 11:13:51 +0100

    tests/runtimes/simple-rofl: Test ROFL upgrade

--- go/oasis-test-runner/scenario/e2e/runtime/rofl.go
+++ go/oasis-test-runner/scenario/e2e/runtime/rofl.go
@@ -1,10 +1,32 @@
 package runtime
 
 import (
+	"context"
+
+	"github.com/oasisprotocol/oasis-core/go/beacon/api"
+	"github.com/oasisprotocol/oasis-core/go/common/version"
+	"github.com/oasisprotocol/oasis-core/go/oasis-test-runner/env"
 	"github.com/oasisprotocol/oasis-core/go/oasis-test-runner/oasis"
 	"github.com/oasisprotocol/oasis-core/go/oasis-test-runner/scenario"
 	"github.com/oasisprotocol/oasis-core/go/runtime/bundle/component"
 )
+
+var (
+	roflScenario         = newROFLScenario("0.0.0")
+	roflUpgradedScenario = newROFLScenario("0.1.0")
+)
+
+func newROFLScenario(version string) TestClientScenario {
+	return NewTestClientScenario([]interface{}{
+		InsertKeyValueTx{"my_key", "my_value", "", 0, 0, encryptedWithSecretsTxKind},
+		GetKeyValueTx{"my_key", "my_value", 0, 0, encryptedWithSecretsTxKind},
+		RemoveKeyValueTx{"my_key", "my_value", 0, 0, encryptedWithSecretsTxKind},
+		GetKeyValueTx{"my_key", "", 0, 0, encryptedWithSecretsTxKind},
+		// Check that the ROFL component wrote the HTTP response into storage.
+		GetKeyValueTx{"rofl_version", version, 0, 0, plaintextTxKind},
+		KeyExistsTx{"rofl_http", 0, 0, plaintextTxKind},
+	})
+}
 
 // ROFL is the runtime with a ROFL component scenario.
 var ROFL scenario.Scenario = newROFL()
@@ -15,14 +37,7 @@ type roflImpl struct {
 
 func newROFL() scenario.Scenario {
 	return &roflImpl{
-		Scenario: *NewScenario("rofl", NewTestClient().WithScenario(NewTestClientScenario([]interface{}{
-			InsertKeyValueTx{"my_key", "my_value", "", 0, 0, encryptedWithSecretsTxKind},
-			GetKeyValueTx{"my_key", "my_value", 0, 0, encryptedWithSecretsTxKind},
-			RemoveKeyValueTx{"my_key", "my_value", 0, 0, encryptedWithSecretsTxKind},
-			GetKeyValueTx{"my_key", "", 0, 0, encryptedWithSecretsTxKind},
-			// Check that the ROFL component wrote the HTTP response into storage.
-			KeyExistsTx{"rofl_http", 0, 0, plaintextTxKind},
-		}))),
+		Scenario: *NewScenario("rofl", NewTestClient().WithScenario(roflScenario)),
 	}
 }
 
@@ -45,5 +60,47 @@ func (sc *roflImpl) Fixture() (*oasis.NetworkFixture, error) {
 		Binaries: sc.ResolveRuntimeBinaries(ROFLComponentBinary),
 	})
 
+	// Prepare upgraded ROFL component in a separate bundle.
+	f.Runtimes[1].Deployments = append(f.Runtimes[1].Deployments, oasis.DeploymentCfg{
+		ValidFrom: api.EpochMax,
+		Components: []oasis.ComponentCfg{
+			{
+				Kind:     component.ROFL,
+				Name:     "test-rofl",
+				Version:  version.Version{Major: 0, Minor: 1, Patch: 0},
+				Binaries: sc.ResolveRuntimeBinaries(ROFLComponentUpgradeBinary),
+			},
+		},
+		ExcludeBundle: true,
+	})
+
 	return f, nil
+}
+
+func (sc *roflImpl) Run(ctx context.Context, childEnv *env.Env) error {
+	// Test ROFL component.
+	if err := sc.StartNetworkAndTestClient(ctx, childEnv); err != nil {
+		return err
+	}
+	if err := sc.WaitTestClientAndCheckLogs(); err != nil {
+		return err
+	}
+
+	// Upgrade ROFL component.
+	sc.Logger.Info("upgrading ROFL component")
+
+	path := sc.Net.Runtimes()[1].BundlePath(1)
+	for _, w := range sc.Net.ComputeWorkers() {
+		ctrl, err := oasis.NewController(w.SocketPath())
+		if err != nil {
+			return err
+		}
+		if err = ctrl.AddBundle(ctx, path); err != nil {
+			return err
+		}
+	}
+
+	// Test upgraded ROFL component.
+	sc.Scenario.TestClient = NewTestClient().WithSeed("seed2").WithScenario(roflUpgradedScenario)
+	return sc.RunTestClientAndCheckLogs(ctx, childEnv)
 }
