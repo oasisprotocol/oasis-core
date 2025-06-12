5dd5426434ea1972c7d2a1f30b6a7025a21b1e5f
Martin Tomazic <martin.tomazic97@gmail.com>
2025-06-13 00:09:07 +0200

    go/storage/mkvs/checkpoint/fileCreator: Use shorter names

--- go/storage/mkvs/checkpoint/file.go
+++ go/storage/mkvs/checkpoint/file.go
@@ -17,13 +17,13 @@ import (
 )
 
 const (
-	chunksDir              = "chunks"
-	checkpointMetadataFile = "meta"
-	checkpointVersion      = 1
+	chunksDirname = "chunks"
+	metaFilename  = "meta"
+	v1            = 1
 
 	// Versions 1 of checkpoint chunks use proofs version 0. Consider bumping
 	// this to latest version when introducing new checkpoint versions.
-	checkpointProofsVersion = 0
+	v1ProofsVersion = 0
 )
 
 type fileCreator struct {
@@ -36,23 +36,23 @@ func (fc *fileCreator) CreateCheckpoint(ctx context.Context, root node.Root, chu
 	defer tree.Close()
 
 	// Create checkpoint directory.
-	checkpointDir := filepath.Join(
+	cpDir := filepath.Join(
 		fc.dataDir,
 		strconv.FormatUint(root.Version, 10),
 		root.Hash.String(),
 	)
-	if err = common.Mkdir(checkpointDir); err != nil {
+	if err = common.Mkdir(cpDir); err != nil {
 		return nil, fmt.Errorf("checkpoint: failed to create checkpoint directory: %w", err)
 	}
 	defer func() {
 		if err != nil {
 			// In case we have failed to create a checkpoint, make sure to clean up after ourselves.
-			_ = os.RemoveAll(checkpointDir)
+			_ = os.RemoveAll(cpDir)
 		}
 	}()
 
 	// Check if the checkpoint already exists and just return the existing metadata in this case.
-	data, err := os.ReadFile(filepath.Join(checkpointDir, checkpointMetadataFile))
+	data, err := os.ReadFile(filepath.Join(cpDir, metaFilename))
 	if err == nil {
 		var existing Metadata
 		if err = cbor.Unmarshal(data, &existing); err != nil {
@@ -62,7 +62,7 @@ func (fc *fileCreator) CreateCheckpoint(ctx context.Context, root node.Root, chu
 	}
 
 	// Create chunks directory.
-	chunksDir := filepath.Join(checkpointDir, chunksDir)
+	chunksDir := filepath.Join(cpDir, chunksDirname)
 	if err = common.Mkdir(chunksDir); err != nil {
 		return nil, fmt.Errorf("checkpoint: failed to create chunk directory: %w", err)
 	}
@@ -96,12 +96,12 @@ func (fc *fileCreator) CreateCheckpoint(ctx context.Context, root node.Root, chu
 
 	// Generate and write checkpoint metadata.
 	meta = &Metadata{
-		Version: checkpointVersion,
+		Version: v1,
 		Root:    root,
 		Chunks:  chunks,
 	}
 
-	if err = os.WriteFile(filepath.Join(checkpointDir, checkpointMetadataFile), cbor.Marshal(meta), 0o600); err != nil {
+	if err = os.WriteFile(filepath.Join(cpDir, metaFilename), cbor.Marshal(meta), 0o600); err != nil {
 		return nil, fmt.Errorf("checkpoint: failed to create checkpoint metadata: %w", err)
 	}
 	return meta, nil
@@ -109,7 +109,7 @@ func (fc *fileCreator) CreateCheckpoint(ctx context.Context, root node.Root, chu
 
 func (fc *fileCreator) GetCheckpoints(_ context.Context, request *GetCheckpointsRequest) ([]*Metadata, error) {
 	// Currently we only support a single version so we report no checkpoints for other versions.
-	if request.Version != checkpointVersion {
+	if request.Version != v1 {
 		return []*Metadata{}, nil
 	}
 
@@ -119,7 +119,7 @@ func (fc *fileCreator) GetCheckpoints(_ context.Context, request *GetCheckpoints
 		versionGlob = strconv.FormatUint(*request.RootVersion, 10)
 	}
 
-	matches, err := filepath.Glob(filepath.Join(fc.dataDir, versionGlob, "*", checkpointMetadataFile))
+	matches, err := filepath.Glob(filepath.Join(fc.dataDir, versionGlob, "*", metaFilename))
 	if err != nil {
 		return nil, fmt.Errorf("checkpoint: failed to enumerate checkpoints: %w", err)
 	}
@@ -143,17 +143,17 @@ func (fc *fileCreator) GetCheckpoints(_ context.Context, request *GetCheckpoints
 
 func (fc *fileCreator) GetCheckpoint(_ context.Context, version uint16, root node.Root) (*Metadata, error) {
 	// Currently we only support a single version.
-	if version != checkpointVersion {
+	if version != v1 {
 		return nil, ErrCheckpointNotFound
 	}
 
-	checkpointFilename := filepath.Join(
+	metaPath := filepath.Join(
 		fc.dataDir,
 		strconv.FormatUint(root.Version, 10),
 		root.Hash.String(),
-		checkpointMetadataFile,
+		metaFilename,
 	)
-	data, err := os.ReadFile(checkpointFilename)
+	data, err := os.ReadFile(metaPath)
 	if err != nil {
 		return nil, ErrCheckpointNotFound
 	}
@@ -167,18 +167,18 @@ func (fc *fileCreator) GetCheckpoint(_ context.Context, version uint16, root nod
 
 func (fc *fileCreator) DeleteCheckpoint(_ context.Context, version uint16, root node.Root) error {
 	// Currently we only support a single version.
-	if version != checkpointVersion {
+	if version != v1 {
 		return ErrCheckpointNotFound
 	}
 
 	versionDir := filepath.Join(fc.dataDir, strconv.FormatUint(root.Version, 10))
-	checkpointDir := filepath.Join(versionDir, root.Hash.String())
-	checkpointFilename := filepath.Join(checkpointDir, checkpointMetadataFile)
-	if err := os.Remove(checkpointFilename); err != nil {
+	cpDir := filepath.Join(versionDir, root.Hash.String())
+	metaPath := filepath.Join(cpDir, metaFilename)
+	if err := os.Remove(metaPath); err != nil {
 		return ErrCheckpointNotFound
 	}
 
-	if err := os.RemoveAll(checkpointDir); err != nil {
+	if err := os.RemoveAll(cpDir); err != nil {
 		return fmt.Errorf("checkpoint: failed to remove checkpoint directory: %w", err)
 	}
 
@@ -206,19 +206,19 @@ func (fc *fileCreator) DeleteCheckpoint(_ context.Context, version uint16, root 
 
 func (fc *fileCreator) GetCheckpointChunk(_ context.Context, chunk *ChunkMetadata, w io.Writer) error {
 	// Currently we only support a single version.
-	if chunk.Version != checkpointVersion {
+	if chunk.Version != v1 {
 		return ErrChunkNotFound
 	}
 
-	chunkFilename := filepath.Join(
+	chunkPath := filepath.Join(
 		fc.dataDir,
 		strconv.FormatUint(chunk.Root.Version, 10),
 		chunk.Root.Hash.String(),
-		chunksDir,
+		chunksDirname,
 		strconv.FormatUint(chunk.Index, 10),
 	)
 
-	f, err := os.Open(chunkFilename)
+	f, err := os.Open(chunkPath)
 	if err != nil {
 		return ErrChunkNotFound
 	}
