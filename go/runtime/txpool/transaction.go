63fd9c34e09934219d4e042e92a195f1cdbbca64
Peter Nose <peter.nose@gmail.com>
2025-11-07 03:35:23 +0100

    go/runtime/txpool: Add checked field to pending check tx

--- go/runtime/txpool/transaction.go
+++ go/runtime/txpool/transaction.go
@@ -4,28 +4,16 @@ import (
 	"github.com/oasisprotocol/oasis-core/go/runtime/host/protocol"
 )
 
-// txCheckFlags are the flags describing how a transaction should be checked.
-type txCheckFlags uint8
-
-const (
-	// txCheckRecheck is a flag indicating that the transaction already passed checking earlier.
-	txCheckRecheck = 1 << 0
-)
-
-func (f txCheckFlags) isRecheck() bool {
-	return (f * txCheckRecheck) != 0
-}
-
 // PendingCheckTransaction is a transaction pending checks.
 type PendingCheckTransaction struct {
 	*TxQueueMeta
 
-	// flags are the transaction check flags.
-	flags txCheckFlags
 	// local indicates whether the transaction came from our own node.
 	local bool
 	// discard indicates whether the transaction should be discarded after validation.
 	discard bool
+	// checked indicates whether the transaction already passed check.
+	checked bool
 	// notifyCh is a channel for sending back the transaction check result.
 	notifyCh chan *protocol.CheckTxResult
 }
