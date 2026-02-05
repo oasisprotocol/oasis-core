18a2c8b283d5b2fd89eb6921fa0b14f3930e060c
Martin Tomazic <martin.tomazic97@gmail.com>
2026-02-05 14:09:01 +0100

    go/oasis-node/cmd/storage: Factor out db providers
    Observe that migrate command now opens runtime light history with
    hasLocalStorage=true. This is intentional, to be consistent with
    the other callsite. When doing migration or offline pruning the
    node clearly needs to have local storage. In practice this is not
    relevant as it only affects behaviour of light history when
    the storage committee worker is running. Furthermore, once we
    move pruning and (re)-indexing of the runtime light history
    this half of the history.History api can be removed, including
    this variable.

---
+++ go/oasis-node/cmd/storage/dbs.go
@@ -0,0 +1,85 @@
+package storage
+
+import (
+	"fmt"
+	"path/filepath"
+
+	cmtCfg "github.com/cometbft/cometbft/config"
+	"github.com/cometbft/cometbft/state"
+	"github.com/cometbft/cometbft/store"
+
+	"github.com/oasisprotocol/oasis-core/go/common"
+	"github.com/oasisprotocol/oasis-core/go/config"
+	"github.com/oasisprotocol/oasis-core/go/consensus/cometbft/abci"
+	cmtCommon "github.com/oasisprotocol/oasis-core/go/consensus/cometbft/common"
+	cmtDB "github.com/oasisprotocol/oasis-core/go/consensus/cometbft/db"
+	runtimeConfig "github.com/oasisprotocol/oasis-core/go/runtime/config"
+	"github.com/oasisprotocol/oasis-core/go/runtime/history"
+	"github.com/oasisprotocol/oasis-core/go/storage/api"
+)
+
+func openConsensusNodeDB(dataDir string) (api.NodeDB, func(), error) {
+	ldb, ndb, _, err := abci.InitStateStorage(
+		&abci.ApplicationConfig{
+			DataDir:             filepath.Join(dataDir, cmtCommon.StateDir),
+			StorageBackend:      config.GlobalConfig.Storage.Backend,
+			MemoryOnlyStorage:   false,
+			ReadOnlyStorage:     false,
+			DisableCheckpointer: true,
+		},
+	)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to initialize ABCI storage backend: %w", err)
+	}
+
+	// Close and Cleanup both only close NodeDB. Still closing both explicitly,
+	// to prevent resource leaks if things change in the future.
+	close := func() {
+		ndb.Close()
+		ldb.Cleanup()
+	}
+
+	return ndb, close, nil
+}
+
+func openConsensusBlockstore(dataDir string) (*store.BlockStore, error) {
+	cmtConfig := cmtCfg.DefaultConfig()
+	cmtConfig.SetRoot(filepath.Join(dataDir, cmtCommon.StateDir))
+
+	dbProvider, err := cmtDB.Provider()
+	if err != nil {
+		return nil, fmt.Errorf("failed to obtain db provider: %w", err)
+	}
+
+	blockstoreDB, err := cmtDB.OpenBlockstoreDB(dbProvider, cmtConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open blockstore: %w", err)
+	}
+	blockstore := store.NewBlockStore(blockstoreDB)
+
+	return blockstore, nil
+}
+
+func openConsensusStatestore(dataDir string) (state.Store, error) {
+	cmtConfig := cmtCfg.DefaultConfig()
+	cmtConfig.SetRoot(filepath.Join(dataDir, cmtCommon.StateDir))
+
+	dbProvider, err := cmtDB.Provider()
+	if err != nil {
+		return nil, fmt.Errorf("failed to obtain db provider: %w", err)
+	}
+	stateDB, err := cmtDB.OpenStateDB(dbProvider, cmtConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open state db: %w", err)
+	}
+	return cmtDB.OpenStateStore(stateDB), nil
+}
+
+func openRuntimeLightHistory(dataDir string, rt common.Namespace) (history.History, error) {
+	rtDir := runtimeConfig.GetRuntimeStateDir(dataDir, rt)
+	history, err := history.New(rt, rtDir, history.NewNonePrunerFactory(), true)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open new light history: %w", err)
+	}
+	return history, nil
+}
